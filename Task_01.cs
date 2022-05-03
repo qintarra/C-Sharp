7ee7e50d99105727dc50b57ad53a1cfdaf6ca56e
Qintarra <brain_damage@ukr.net>
2022-05-03 15:08:18 +0300

    fist task

---
+++ Task_01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task01
+
+{
+    //Calculate the arithmetic mean of two numbers
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, secondValue;
+
+            Console.WriteLine("Enter 1st number");
+
+            firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number");
+
+            secondValue = double.Parse(Console.ReadLine());
+
+            double result = (firstValue + secondValue) / 2;
+
+            Console.WriteLine("The arithmetic mean of your numbers is " + result);
+
+        }
+    }
+}
