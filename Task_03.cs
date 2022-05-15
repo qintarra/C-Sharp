12020a583b20b66c7feac2cf0b35bf1396d2efae
Qintarra <brain_damage@ukr.net>
2022-05-15 17:32:52 +0300

    task 2 added

--- Task_03.cs
+++ Task_03.cs
@@ -11,5 +11,16 @@ namespace Task_03
 
             Console.WriteLine($"Sum = {sum}, Product = {product}");
         }
+		
+		public static void Task2(int n)
+		//A three-digit number n is given. Find the sum and the product of its digits.
+		//Example: n = 450  =>  sum = 9,  product = 0
+				 //n = 817  =>  sum = 16, product = 56
+        {
+            int sum = (n/100) + ((n/10)%10) + (n%10);
+            int product = (n/100) * ((n/10)%10) * (n%10);
+
+            Console.WriteLine($"Sum = {sum}, Product = {product}");
+        }
     }
 }
