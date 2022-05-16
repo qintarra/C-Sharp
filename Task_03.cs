488aa1f0ce36a38a9145c99dd1c6d171c6959580
Qintarra <brain_damage@ukr.net>
2022-05-16 15:36:20 +0300

    comments update

--- Task_03.cs
+++ Task_03.cs
@@ -8,7 +8,7 @@ namespace Task_03
 		//Example n = 45  =>  sum = 9,  product = 20  
 
         {  
-			int sum = (n / 10) + (n %1 0);  
+            int sum = (n / 10) + (n %1 0);  
 
             int product = (n / 10) * (n % 10);  
   
@@ -19,10 +19,11 @@ namespace Task_03
 
 		//A three-digit number n is given. Find the sum and the product of its digits.  
 		//Example n = 450  =>  sum = 9,  product = 0  
-				//n = 817  =>  sum = 16, product = 56  
+			//n = 817  =>  sum = 16, product = 56  
 
         {  
             int sum = (n / 100) + ((n / 10) % 10) + (n % 10);  
+
             int product = (n / 100) * ((n / 10) % 10) * (n % 10);  
   
             Console.WriteLine($"Sum = {sum}, Product = {product}");  
@@ -32,8 +33,8 @@ namespace Task_03
 
 		//A three-digit number n is given. The first left digit was crossed out and ascribed to the right. Find the obtained number.  
 		//Example n = 450  =>  result = 504  
-				//n = 807  =>  result = 78  
-				//n = 695  =>  result = 956  
+			//n = 807  =>  result = 78  
+			//n = 695  =>  result = 956  
 
         {  
             int result = ((n / 10) % 10) * 100 + (n % 10) * 10 + (n / 100);
@@ -45,8 +46,8 @@ namespace Task_03
 
 		//A two-digit number n is given. Find the number that was obtained after reordering the initial numbers.  
 		//Example n = 45  result = 54  
-				//n = 80  result = 8  
-				//n = 69  result = 96  
+			//n = 80  result = 8  
+			//n = 69  result = 96  
 
         {  
             int result = 10 * (n % 10) + (n / 10);  
@@ -58,8 +59,8 @@ namespace Task_03
 
 		//A three-digit number n is given. Find the number that was obtained by processing the initial digit from the right to the left.  
 		//Example n = 450  result = 54  
-				//n = 807  result = 708  
-				//n = 695  result = 596  
+			//n = 807  result = 708  
+			//n = 695  result = 596  
 
 		{  
             int result = 100 * (n % 10) + 10 * (n % 100 / 10) + (n / 100);  
@@ -71,8 +72,8 @@ namespace Task_03
 
 		//A three-digit number n is given. The first right digit was crossed out and ascribed to the left. Find the obtained number.  
 		//Example n = 450  result = 45  
-				//n = 807  result = 780  
-				//n = 695  result = 569  
+			//n = 807  result = 780  
+			//n = 695  result = 569  
 
         {  
             int result = 100 * (n % 10) + 10 * (n / 100) + (n % 100 / 10);  
