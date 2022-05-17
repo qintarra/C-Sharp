5d287acfb08a6f80a4f848b5ed6dd58088ed8a4c
Qintarra <brain_damage@ukr.net>
2022-05-17 15:28:45 +0300

    6th task added

--- Task_04.cs
+++ Task_04.cs
@@ -71,5 +71,21 @@
 
             Console.WriteLine(Math.Round(distance, 2));
         }
+
+        public static void Task6(double s, double t, double v1, double v2)
+
+        /* The speed of the first car is V1 km/h, the speed of the second car is V2 km/h, the distance between them is S km. 
+         * Find the distance between cars after T hours, if the cars are distancing from each other. 
+         * The given distance is equal the sum of the initial distance + the general way, which was covered by cars; the general way = time*total speed.
+         * Example: V1 = 90 km/h; V2 = 110 km/h; S = 65 km; T = 3 h  =>  Distance = 665 km 
+         *          V1 = 65.5 km/h; V2 = 90.4 km/h; S = 20.9 km; T = 1.5 h  =>  Distance = 254.75 km 
+         *          V1 = 70 km/h; V2 = 85.6 km/h; S = 32.6 km; T = 2 h  =>  Distance = 343.8 km  
+         */
+
+        {
+            double distance = v1 * t + v2 * t + s;
+
+            Console.WriteLine(Math.Round(distance, 2));
+        }
     }
 }
