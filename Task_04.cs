42822ad43013ce902d8e370d9160638413b9e76e
Qintarra <brain_damage@ukr.net>
2022-05-17 14:49:24 +0300

    task 04 added

---
+++ Task_04.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_04
+{
+    public static class Tasks
+    {   
+        public static void Task1(int a)
+
+        //Find square's area following the formula: S = a²
+        //Example: а = 4   area = 16
+
+        {
+            int perimeter = (4 * a);
+				
+            Console.WriteLine($"P = {perimeter}");
+        }
+    }
+}
