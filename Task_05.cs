633ad2b5e9a0fc6b0bf376b1ee478a3931f13c5a
Qintarra <brain_damage@ukr.net>
2022-05-18 11:31:27 +0300

    added task_05

---
+++ Task_05.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_05
+{
+    public static class Tasks
+    {
+        public static int Task1(int a)
+        {
+            if (a > 0)
+            {
+                return a + 1;
+            }
+            else
+            {
+                return a - 2;
+            }
+        }
+    }
+}
+
