22a49e031d62811265c8760af034c23735cee03f
Qintarra <brain_damage@ukr.net>
2022-05-18 12:14:17 +0300

    description

--- Task_05.cs
+++ Task_05.cs
@@ -3,6 +3,13 @@
     public static class Tasks
     {
         public static int Task1(int a)
+
+        /* Given an integer A. If A is positive, then add 1 to it. Otherwise subtract 2 from it.  
+         * Example
+         * A = 19  =>  A = 20
+         * A = -13  =>  A = -15
+         */
+
         {
             if (a > 0)
             {
