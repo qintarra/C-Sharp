6cab142c7893f77fccee27715326e9cc0075f308
Qintarra <brain_damage@ukr.net>
2022-05-19 10:29:16 +0300

    second task

--- Task_06.cs
+++ Task_06.cs
@@ -4,11 +4,11 @@
     {
         public static int Task1(int a,  int b, int c)
 		
-		/* Given three unequal numbers A, B, C. Find the sum of the two largest of them
-		 * Example: A = 11;  B = 34 ; C = 7 =>  Result = 45
-		 *			A = 17;  B = -12 ; C = -6 =>  Result = 11
-		 *			A = 5;  B = 0 ; C = 13 =>  Result = 18
-		 */
+		    /* Given three unequal numbers A, B, C. Find the sum of the two largest of them
+		    * Example: A = 11;  B = 34 ; C = 7 =>  Result = 45
+		    *			A = 17;  B = -12 ; C = -6 =>  Result = 11
+		    *			A = 5;  B = 0 ; C = 13 =>  Result = 18
+		    */
 		
         {
             var result = 0;
@@ -30,6 +30,24 @@
             }
             return result;
         }
-    } 
+
+        public static (int a, int b, int c) Task2(int a, int b, int c)
+
+            /* Given three real type values A, B, C. Rewrite the values of the variables so that A has the smaller of the values, and C has the larger one.
+             * Example: A = 10;  B = -15 ; C = 7 =>  A = -15;  B = -15 ; C = 10
+             *          A = -1;  B = 10 ; C = 5 =>  A = -1;  B = 10 ; C = 10
+             *          A = -17;  B = -2 ; C = -14 =>  A = -17;  B = -2 ; C = -2
+             */
+
+        {
+            int min = Math.Min(Math.Min(a, b), c);
+            int max = Math.Max(Math.Max(a, b), c);
+            a = min;
+            c = max;
+            return (a, b, c);
+        }
+
+
+    }
 }
 
