3d35aaa9ae9edb60a6769fd4cc4ab286cdd39b40
Qintarra <brain_damage@ukr.net>
2022-05-20 15:22:40 +0300

    second task

--- Task_07.cs
+++ Task_07.cs
@@ -4,8 +4,8 @@ namespace Task_07
     {
         public static int Task1(int n, int m)
 		
-		// Get the sum of even numbers in the interval from n to m including.
-		// Example: n = 4, m = 9   sum = 18 
+	// Get the sum of even numbers in the interval from n to m including.
+	// Example: n = 4, m = 9   sum = 18 
 		
         {
             int sum = 0;
@@ -16,6 +16,23 @@ namespace Task_07
                 {
                     sum += n;
                 }
+                n++;
+            }
+
+            return sum;
+        }
+		
+		public static int Task2(int n, int m)
+		
+	// Get the sum of odd numbers in the interval from n to m including.
+	// Example: n = 4, m = 10   sum = 21
+		
+        {
+            int sum = 0;
+
+            while(n <= m)
+            {
+                sum += (n % 2 == 1) ? n : 0;
                 n++;
             }
 
