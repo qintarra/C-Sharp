57644d9a261f2f61987a4f1c6b4e36dfa86bfc52
Qintarra <brain_damage@ukr.net>
2022-05-22 11:06:31 +0300

    notes added

---
+++ Notes.cs
@@ -0,0 +1,41 @@
+// My notes from books and lectures.
+
+using System;
+
+namespace Notes_01
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			
+			int number;
+			number = 10;
+			double ak = 5.45;
+			string s = "any text";
+			char b = 'k';
+			bool t = true;
+			
+			Console.WriteLine(number);
+			Console.WriteLine(ak);
+			Console.WriteLine(s);
+			Console.WriteLine(b);
+			Console.WriteLine(t);
+			
+			int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine(z);
+            Console.WriteLine(x + " + " + y + " = " + z);
+            Console.WriteLine("{0} = {1} + {2}", z, y, x);
+            Console.WriteLine($"{x} + {y} = {z}");
+      
+        }
+    }
+}
+
+		
+		
+		
