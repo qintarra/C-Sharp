fbd4f90deb93887ac31d6d50419bb32b476d6628
Qintarra <brain_damage@ukr.net>
2022-05-22 10:30:29 +0300

    09 added

---
+++ Task_09.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+
+                try
+                {
+                    Console.WriteLine("Enter first number");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter second number");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error. Enter numbers");
+                    Console.ReadLine();
+                    continue;
+                }
+                
+                Console.WriteLine("Select operation: '+', '-', '*', '/'");
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondValue);
+                        break;
+                    case "-":
+                        Console.WriteLine(firstValue - secondValue);
+                        break;
+                    case "*":
+                        Console.WriteLine(firstValue * secondValue);
+                        break;
+                    case "/":
+                        if (secondValue == 0)
+                        {
+                            Console.WriteLine("Zero Division Error");
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstValue / secondValue);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error. Unknown Action.");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
