e5aabcc9b234e184e83e5dd6d8d026d41ecbf3e0
Qintarra <brain_damage@ukr.net>
2022-05-22 11:09:28 +0300

    update

--- Notes.cs
+++ Notes.cs
@@ -12,17 +12,16 @@ namespace Notes_01
             Console.WriteLine("Hello World!");
 			
 			int number;
-			number = 10;
-			double ak = 5.45;
-			string s = "any text";
-			char b = 'k';
-			bool t = true;
-			
-			Console.WriteLine(number);
-			Console.WriteLine(ak);
-			Console.WriteLine(s);
-			Console.WriteLine(b);
-			Console.WriteLine(t);
+            number = 10;
+            double ak = 5.45;
+            string s = "any text";
+            char b = 'k';
+            bool t = true;
+            Console.WriteLine(number);
+            Console.WriteLine(ak);
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+            Console.WriteLine(t);
 			
 			int x = 10;
             int y = 20;
@@ -31,6 +30,16 @@ namespace Notes_01
             Console.WriteLine(x + " + " + y + " = " + z);
             Console.WriteLine("{0} = {1} + {2}", z, y, x);
             Console.WriteLine($"{x} + {y} = {z}");
+			
+			// Two methods for converting a string to number
+            int x = int.Parse(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = x + y; // Operators: +, -, *, /, %
+			// % operator - reminder of a division
+
+            Console.WriteLine($"{x} + {y} = {z}");
+			
+
       
         }
     }
