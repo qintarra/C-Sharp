76c0ce788a0d7fd1f308d426e6922e9fefec650c
Qintarra <brain_damage@ukr.net>
2022-05-24 12:10:37 +0300

    switch/case

--- Notes/Notes_02.cs
+++ Notes/Notes_02.cs
@@ -15,6 +15,30 @@ namespace Notes_02
             int mid = a + b + c - max - min;
 
             Console.WriteLine(Convert.ToInt32($"{max}{mid}{min}"));
+			
+            //2.1
+            Console.WriteLine("Please, enter a number from 1 to 4");
+            int x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("You typed 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You typed 2");
+                    break;
+                case 3:
+                    Console.WriteLine("You typed 3");
+                    break;
+                case 4:
+                    Console.WriteLine("You typed 4");
+                    break;
+                default:
+                    Console.WriteLine("You are mistaken");
+                    break;
+            }
+            Console.WriteLine("Continue...");
 
         }
 
