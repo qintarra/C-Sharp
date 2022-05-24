f679fcb36d682081debd9f1d287ad62252731de6
Qintarra <brain_damage@ukr.net>
2022-05-24 12:13:46 +0300

    switch/case_2

--- Notes/Notes_02.cs
+++ Notes/Notes_02.cs
@@ -39,6 +39,25 @@ namespace Notes_02
                     break;
             }
             Console.WriteLine("Continue...");
+			
+			//2.2
+			Console.WriteLine("Where do we go from here? (right, left, straight)");
+            string direction = Console.ReadLine();
+            switch (direction)
+            {
+                case "right":
+                    Console.WriteLine("Why is the path unclear");
+                    break;
+                case "left":
+                    Console.WriteLine("When we know hope is near?");
+                    break;
+                case "straight":
+                    Console.WriteLine("When does the end appear?");
+                    break;
+                default:
+                    Console.WriteLine("You can tell the end is near");
+                    break;
+            }
 
         }
 
