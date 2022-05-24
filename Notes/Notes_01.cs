bc4c07a0a51a64bc81ad383251bb8fc5f9a53f25
Qintarra <brain_damage@ukr.net>
2022-05-24 12:40:00 +0300

    variables fixed

--- Notes/Notes_01.cs
+++ Notes/Notes_01.cs
@@ -12,70 +12,75 @@ namespace Notes_01
             number = 10;
             double ak = 5.45;
             string s = "any text";
-            char b = 'k';
+            char r = 'k';
             bool t = true;
             Console.WriteLine(number);
             Console.WriteLine(ak);
             Console.WriteLine(s);
-            Console.WriteLine(b);
+            Console.WriteLine(r);
             Console.WriteLine(t);
 			
-            int x = 10;
-            int y = 20;
-            int z = x + y;
-            Console.WriteLine(z);
-            Console.WriteLine(x + " + " + y + " = " + z);
-            Console.WriteLine("{0} = {1} + {2}", z, y, x);
-            Console.WriteLine($"{x} + {y} = {z}");
+			
+            int g = 10;
+            int f = 20;
+            int n = g + f;
+            Console.WriteLine(g);
+            Console.WriteLine(g + " + " + f + " = " + n);
+            Console.WriteLine("{0} = {1} + {2}", n, f, g);
+            Console.WriteLine($"{g} + {f} = {n}");
+			
 			
             // Two methods for converting a string to number
-            int x = int.Parse(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
-            int z = x + y; // Operators: +, -, *, /, %
+            int i = int.Parse(Console.ReadLine());
+            int e = Convert.ToInt32(Console.ReadLine());
+            int h = i + e; // Operators: +, -, *, /, %
             // % operator - reminder of a division
 
-            Console.WriteLine($"{x} + {y} = {z}");
+            Console.WriteLine($"{i} + {e} = {h}");
+			
 			
             // Mathematical functions. Math class methods
-            double x = double.Parse(Console.ReadLine());
-            double y = Convert.ToDouble(Console.ReadLine());
+            double k = double.Parse(Console.ReadLine());
+            double l = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("sin({0}) = {1}",x,Math.Sin(x));
-            Console.WriteLine("{0}^2 = {1}",y,Math.Pow(y, 2));
-            Console.WriteLine("square root of {0} = {1}",x,Math.Sqrt(x));   
-            Console.WriteLine("minimum = {0}",Math.Min(x,y));
-            Console.WriteLine("maximum = {0}",Math.Max(x, y));
+            Console.WriteLine("sin({0}) = {1}",k,Math.Sin(k));
+            Console.WriteLine("{0}^2 = {1}",l,Math.Pow(l, 2));
+            Console.WriteLine("square root of {0} = {1}",k,Math.Sqrt(k));   
+            Console.WriteLine("minimum = {0}",Math.Min(k,l));
+            Console.WriteLine("maximum = {0}",Math.Max(k, l));
+			
 			
             //Conditional operator full form
 
-            int x = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
 
-            if (x % 2 == 0)
+            if (m % 2 == 0)
             {
-                Console.WriteLine($"Number {x} is even");
+                Console.WriteLine($"Number {m} is even");
             }
             else
             {
-                Console.WriteLine($"Number {x} is odd");
+                Console.WriteLine($"Number {m} is odd");
             }
+			
       
             //Conditional operator short form
 
-            int x = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
 
-            if (x % 2 == 0)
+            if (p % 2 == 0)
             {
-                Console.WriteLine($"Number {x} is even");
+                Console.WriteLine($"Number {p} is even");
             }
 
-            if (x % 2 != 0)
+            if (p % 2 != 0)
             {
-                Console.WriteLine($"Number {x} is odd");
+                Console.WriteLine($"Number {p} is odd");
             }
 
 
 
-            double a, b, c, d, x1, x2, x;
+            double a, b, c, d, x1, x2, x3;
             a = double.Parse(Console.ReadLine());
             b = double.Parse(Console.ReadLine());
             c = double.Parse(Console.ReadLine());
@@ -90,15 +95,14 @@ namespace Notes_01
             {
                 if (d == 0)
                 {
-                    x = -b / (2 * a);
-                    Console.WriteLine($"x = {x}");
+                    x3 = -b / (2 * a);
+                    Console.WriteLine($"x3 = {x3}");
                 }
                 else
                 {
                     Console.WriteLine("No solutions");
                 }
             }
-
         }
     }
 }
