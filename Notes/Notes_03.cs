fcb06a10b082fe34e9228924ee48fe4e58c5ae1f
Qintarra <brain_damage@ukr.net>
2022-05-25 22:03:33 +0300

    03 added

---
+++ Notes/Notes_03.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Notes_03
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int max;
+
+            if (x > y)
+            {
+                max = x;
+            }
+            else
+            {
+                max = y;
+            }
+            Console.WriteLine($"Max = {max}");
+
+            max = x > y ? x : y; //solution with ternary operator
+
+            Console.WriteLine($"Max = {max}");
+
+            Console.WriteLine($"max = {(x > y ? x : y)}"); //another solution
+
+           
+
+
+        }
+
+    }
+}
