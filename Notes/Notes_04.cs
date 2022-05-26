1f49a4ddf723c6806241ef0aac896c43777e65bf
Qintarra <brain_damage@ukr.net>
2022-05-26 11:16:50 +0300

    array elements

--- Notes/Notes_04.cs
+++ Notes/Notes_04.cs
@@ -1,7 +1,6 @@
 using System;
 
 namespace Notes_04
-
 {
     public static class Program
     {
@@ -11,6 +10,14 @@ namespace Notes_04
             int[] m = { 2, 1, 0, 3, 9 };
 
             Console.WriteLine($"Element as index 0 = {m[0]}");
+			
+            //Displaying array elements to string separated by space
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{m[i]} ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
