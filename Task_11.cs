81e0c6aef8140eb5a0058356f0967bc06b45958d
Qintarra <brain_damage@ukr.net>
2022-05-26 11:44:59 +0300

    sum in array

--- Task_11.cs
+++ Task_11.cs
@@ -4,7 +4,8 @@ namespace Task_11
 {
     public static class Tasks
     {
-		//One-dimensional array consists of integer elements. Calculate the sum of array elements greater than a.
+        //One-dimensional array consists of integer elements. Calculate the sum of array elements greater than a.
+        //Example: array = { 25, -6, 59, 120 }, a = 25  =>  result = 179
 		
         public static int Task1(int[] array, int a)
         {
@@ -19,6 +20,25 @@ namespace Task_11
 
             return result;
         }
+		
+        //Calculate the sum of negative elements of the array.
+        //Example: array = { -25, -6, 59, 120 }  =>  result = -31
+		
+        public static int Task2(int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    result += array[i];
+                }
+            }
+
+            return result;
+        }
+
+
 
         
     }
