2514f63be5c9ffd9b06241790ec0e1124d602e0b
Qintarra <brain_damage@ukr.net>
2022-05-26 11:13:37 +0300

    04

---
+++ Notes/Notes_04.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Notes_04
+
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialization with a list of values
+            int[] m = { 2, 1, 0, 3, 9 };
+
+            Console.WriteLine($"Element as index 0 = {m[0]}");
+        }
+    }
+}
