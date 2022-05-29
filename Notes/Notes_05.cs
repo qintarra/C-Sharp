c07ce4e80498bbfa02ddeddb94519995291c446d
Qintarra <brain_damage@ukr.net>
2022-05-29 12:21:09 +0300

    05 added

---
+++ Notes/Notes_05.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Notes_04
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 3, 6, 1},
+                           {4, 2, 7 },
+                           {9, 0, 8 }};
+
+            Console.WriteLine(arr[0, 1]);
+            Console.WriteLine(arr[2, 0]);
+
+            Console.WriteLine("Enter number of rows");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of columns");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] mas = new int[n, m];
+            Console.WriteLine($"Enter {n*m} items");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mas[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+		}
+	}
+}
