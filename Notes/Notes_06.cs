7173f4519ceaad61e1b7ef4a9dc7b64440b50874
Qintarra <brain_damage@ukr.net>
2022-05-30 12:34:28 +0300

    random class

---
+++ Notes/Notes_06.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Notes_06
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+			//Random class and filling the array with random numbers
+            Random r = new Random();
+            int[] mas = new int[10];
+
+            for (int i = 0; i < 10; i++)
+                mas[i] = r.Next(0, 11);
+
+            Console.WriteLine("Random array");
+            foreach (int x in mas)
+            {
+                Console.Write($"{x} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sum = {0}", mas.Sum());
+
+            Console.WriteLine("Min = {0} Max = {1}", mas.Min(), mas.Max());
+            
+        }
+    }
+}
