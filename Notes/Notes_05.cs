1b41dc4eb42f161783a55a075b999d065320cac7
Qintarra <brain_damage@ukr.net>
2022-05-30 12:31:05 +0300

    var fix

--- Notes/Notes_05.cs
+++ Notes/Notes_05.cs
@@ -7,12 +7,12 @@ namespace Notes_05
         static void Main(string[] args)
         {
 			//Two-dimensional array
-            int[,] arr = { { 3, 6, 1},
+            int[,] array = { { 3, 6, 1},
                            {4, 2, 7 },
                            {9, 0, 8 }};
 
-            Console.WriteLine(arr[0, 1]);
-            Console.WriteLine(arr[2, 0]);
+            Console.WriteLine(array[0, 1]);
+            Console.WriteLine(array[2, 0]);
 
             Console.WriteLine("Enter number of rows");
             int n = int.Parse(Console.ReadLine());
@@ -54,17 +54,17 @@ namespace Notes_05
 			
             //Jaggad arrays
             int[][] mas = new int[3][];
-            mas[0] = new int[2]; //allocate memory for the first subarray
-            mas[1] = new int[4]; //allocate memory for the second subarray
-            mas[2] = new int[3]; //allocate memory for the third subarray
+            arr[0] = new int[2]; //allocate memory for the first subarray
+            arr[1] = new int[4]; //allocate memory for the second subarray
+            arr[2] = new int[3]; //allocate memory for the third subarray
 			
             //Entering values from the keyboard
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine("Enter {0} row", i + 1);
-                for (int j = 0; j < mas[i].Length; j++)
+                for (int j = 0; j < arr[i].Length; j++)
                 {
-                    mas[i][j] = Convert.ToInt32(Console.ReadLine());
+                    arr[i][j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
 			
@@ -72,36 +72,36 @@ namespace Notes_05
             Console.WriteLine("Jagged array:");
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < mas[i].Length; j++)
+                for (int j = 0; j < arr[i].Length; j++)
                 {
-                    Console.Write($"{ mas[i][j]}");
+                    Console.Write($"{ arr[i][j]}");
                 }
                 Console.WriteLine();
             }
 			
             //Finding sum in the jagged array through 'for' cycle
-            int sum = 0;
+            int sum1 = 0;
             for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < mas[i].Length; j++)
+                for (int j = 0; j < arr[i].Length; j++)
                 {
-                    sum += mas[i][j];
+                    sum1 += arr[i][j];
                 }
             }
 			
-            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Sum = {sum1}");
 			
             //Finding sum in the jagged array through 'foreach' cycle
-            sum = 0;
-            foreach (int[] y in mas)
+            sum1 = 0;
+            foreach (int[] y in arr)
             {
                 foreach (int x in y)
                 {
-                    sum += x;
+                    sumq += x;
                 }
             }
 
-            Console.WriteLine($"Sum = {sum} ");
+            Console.WriteLine($"Sum = {sum1} ");
         }
     }
 }
