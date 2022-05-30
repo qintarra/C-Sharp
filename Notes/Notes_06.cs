61d972bc5f13ba11fad752921624ce32572af936
Qintarra <brain_damage@ukr.net>
2022-05-30 12:37:04 +0300

    reverse

--- Notes/Notes_06.cs
+++ Notes/Notes_06.cs
@@ -6,7 +6,7 @@ namespace Notes_06
     {
         static void Main(string[] args)
         {
-			//Random class and filling the array with random numbers
+            //Random class and filling the array with random numbers
             Random r = new Random();
             int[] mas = new int[10];
 
@@ -23,6 +23,15 @@ namespace Notes_06
             Console.WriteLine("Sum = {0}", mas.Sum());
 
             Console.WriteLine("Min = {0} Max = {1}", mas.Min(), mas.Max());
+			
+			
+            Array.Reverse(mas);
+            Console.WriteLine("Reversed array");
+            foreach (int x in mas)
+            {
+                Console.Write($"{x} ");
+            }
+            Console.WriteLine();
             
         }
     }
