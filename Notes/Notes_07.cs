220ecd8346d022467a90705f6238604fc7055a40
Qintarra <brain_damage@ukr.net>
2022-05-31 11:32:59 +0300

    create 2nd array

--- Notes/Notes_07.cs
+++ Notes/Notes_07.cs
@@ -14,13 +14,20 @@ namespace Notes_07
             arr1[1] = 2;
             arr1[2] = 3;
 			
-			//Displaying the first array
+            //Displaying the first array
             Console.WriteLine("First array");
             foreach (int x in arr1)
             {
                 Console.WriteLine(x);
             }
             
+            //Create the second array, copy the first into it
+            int[] arr2 = arr1;
+            Console.WriteLine("Second array");
+            foreach (int x in arr2)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
