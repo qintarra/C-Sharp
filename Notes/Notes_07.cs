13651abb5880a0c948cca94f4eaa846b5499fd1f
Qintarra <brain_damage@ukr.net>
2022-05-31 11:29:17 +0300

    create 1st array

---
+++ Notes/Notes_07.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Notes_07
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+			//Remember about the reference type of arrays
+			
+			//Create the first array of the integers
+            int[] mas1 = new int[3];
+            mas1[0] = 1;
+            mas1[1] = 2;
+            mas1[2] = 3;
+            
+        }
+    }
+}
