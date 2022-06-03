cac3eca1a9beae1f1850a5f54e8f4358dd429de6
Qintarra <brain_damage@ukr.net>
2022-06-03 09:57:33 +0300

    task 13 added

---
+++ Task_13.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Task_13
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Filling a two-dimensional array with random numbers
+            int[,] myArray = new int[10, 6];
+
+            Random random = new Random();
+
+            
+        }
+    }
+}
