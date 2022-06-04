2c6a022122349082102bb02d6962ddbbe69a2eb3
Qintarra <brain_damage@ukr.net>
2022-06-04 11:19:44 +0300

    filling array from kb

--- Task_13.cs
+++ Task_13.cs
@@ -28,6 +28,29 @@ namespace Task_13
                 Console.WriteLine();
             }
 			
+            //Filling a two-dimensional array from the keyboard
+            int[,] array = new int[2, 3];
+
+            Console.WriteLine("Enter an array elements:");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    
+                    array[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
