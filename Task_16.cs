8f65f3e4df032eabab1f76e660e3fbb7f5aff95c
Qintarra <brain_damage@ukr.net>
2022-06-06 12:38:04 +0300

    2nd task

--- Task_16.cs
+++ Task_16.cs
@@ -8,7 +8,7 @@ namespace Task_16
 {
     public static class Tasks
     {
-        /*Find the sum of the minimum elements of all columns.
+        /*Two-dimensional array consists of integer elements. Find the sum of the minimum elements of all columns.
         Example:
                 array: { { 21, 12, 7 },
                          { -3, -14, 3 },
@@ -37,6 +37,26 @@ namespace Task_16
 
             return sum;
         }
+		
+        /*Calculate the product of the elements of the main diagonal.
+        Example:
+                array: { { 5, 7, 50 },
+                         { 10, 30, 305 },
+                         { -10, 50 ,60 } }
+
+                product = 9000
+        */
+        public static int Task2(int[,] array)
+        {
+            int product = 1;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                
+                product *= array[i, i];
+
+            return product;
+        }
+
 	}
 }
 	
