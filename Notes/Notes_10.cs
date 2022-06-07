e53478f890325dcb01d9ebfbe5a835a2e5d742bf
Qintarra <brain_damage@ukr.net>
2022-06-07 11:56:13 +0300

    enum

---
+++ Notes/Notes_10.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Lection1_end
+{
+    enum StaffPosition
+    {
+        Junior,
+        Middle,
+        Senior,
+        Lead
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Career path");
+            StaffPosition sp;
+            sp = StaffPosition.Junior;
+            Console.WriteLine($"Current position: {sp}");
+
+            sp++;
+            Console.WriteLine($"Current position: {sp}");
+
+            Console.WriteLine("Salary growth");
+            int salary = 400;
+            for (StaffPosition p = StaffPosition.Junior; p <= StaffPosition.Lead; p++)
+            {
+                Console.WriteLine($"Position: {p}, Salary: {salary} $");
+                salary += 550;
+            }
+        }
+    }
+}
