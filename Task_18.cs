7a0735d065768b0cc19c38667def8446f4b7bd61
Qintarra <brain_damage@ukr.net>
2022-06-09 09:58:50 +0300

    task 18 updated

--- Task_18.cs
+++ Task_18.cs
@@ -4,27 +4,7 @@ namespace Task_18
 {
     internal class Program
     {
-        //Write a method that displays a string
-        static void PrintString (string symbol, uint symbolCount)
-        {
-            for (int i = 0; i < symbolCount; i++)
-            {
-                Console.WriteLine(symbol);
-            }
-        }
-
-        static void Main(string [] args)
-        {
-            Console.WriteLine("Enter a symbols:");
-            string symbol = Console.ReadLine();
-
-            Console.WriteLine("Enter the number of symbols:");
-            uint symbolCount = uint.Parse(Console.ReadLine());
-
-            PrintString(symbol, symbolCount);
-        }
-		
-		//Write a method to find the indexes of array elements (typeof int)
+        //Write a method to find the indexes of array elements (typeof int)
         //The method must return the index of the first found element.
         static int IndexOf (int [] array, int value)
         {
