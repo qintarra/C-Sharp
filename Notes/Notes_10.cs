762eee560268d85473b8aac9b46ae6a260cb4d13
Qintarra <brain_damage@ukr.net>
2022-06-10 11:08:11 +0300

    ref

--- Notes/Notes_10.cs
+++ Notes/Notes_10.cs
@@ -32,6 +32,7 @@ namespace Notes_10.cs
         }
 		
 		//10.2
+		
         static void Hello(string name)
         {
             Console.WriteLine($"Hello, {name}!");
@@ -43,6 +44,7 @@ namespace Notes_10.cs
         }
 		
 		//10.3
+		
         static void Sum1(int x, int y)
         {
             Console.WriteLine(x+y);
@@ -61,6 +63,27 @@ namespace Notes_10.cs
 
             //int c = Sum2(a, b);
             Console.WriteLine(Sum2(a,b));
+        }
+		
+        //10.4
+		
+        //Passing parameters by reference and ref modifier
+    
+        //Method definition
+        static void Sum(ref int x, int y)
+        {
+            x += y;
+            Console.WriteLine($"The value in the method {x}");
+        }
+
+        static void Main(string [] args)
+        {
+            //The argument passed to the ref parameter
+            //Must be initialized first
+            int x = 10;
+            int y = 15;
+            Sum(ref x, y); //method call
+            Console.WriteLine($"The value after method {x}");
         }
     }
 }
