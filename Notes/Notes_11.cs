ed527d55281a2633e55324bb1b550a2b8ec532ab
Qintarra <brain_damage@ukr.net>
2022-06-11 10:46:06 +0300

    optional params

--- Notes/Notes_11.cs
+++ Notes/Notes_11.cs
@@ -4,6 +4,7 @@ namespace Notes_11.cs
 {
     class Program
     {
+        //11.1
         static void Sum(int a, in int b, ref int c, out int d)
         {
             Console.WriteLine($"The sum before change is {a + b + c}");
@@ -36,7 +37,29 @@ namespace Notes_11.cs
             Console.WriteLine($"The value of the third parameter {z}");
             Console.WriteLine($"The value of the fourth parameter {s}");
         }
+		
+        //11.2
 
-			
-	}
+       /* C# allows an optional parameters.
+        * For such parameters you must declare a default value.
+        * Also keep in min thet after an optional parameters
+        * all subsequent parameters must also be optional
+        */
+    
+        static int Sum(int x, int y, int z = 3, int s = 4)
+        {
+            return x + y + z + s;
+        }
+        static void Main(string [] args)
+        {
+            //Since the last two parameters are declared optional,
+            //then we can omit one or both of them
+            int sum1 = Sum(1, 2);
+            Console.WriteLine(sum1);
+            int sum2 = Sum(1, 2, 10);
+            Console.WriteLine(sum2);
+            int sum3 = Sum(10, 20, 30, 40);
+            Console.WriteLine(sum3);
+        }
+    }
 }
