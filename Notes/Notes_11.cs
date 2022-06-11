3a3f9ec1341f58c7ca74c593e79036725d938575
Qintarra <brain_damage@ukr.net>
2022-06-11 10:47:37 +0300

    named params

--- Notes/Notes_11.cs
+++ Notes/Notes_11.cs
@@ -61,5 +61,27 @@ namespace Notes_11.cs
             int sum3 = Sum(10, 20, 30, 40);
             Console.WriteLine(sum3);
         }
+		
+        //11.3
+		
+        //Named parameters
+        static int Sum(int x, int y, int z = 3, int s = 4)
+        {
+            return x + y + z + s;
+        }
+        static void Main(string [] args)
+        {
+            int sum1 = Sum(x : 1, y : 2);
+            Console.WriteLine(sum1);
+
+            /* In the previous example, when calling methods, the value for parameters
+             * passed in the order in which these parameters were declared in the method.
+             * But we can break his order by using named parameters
+             */
+            //Optional parameter z uses a default value
+            int sum2 = Sum(y: 2, x: 1, s: 10);
+            Console.WriteLine(sum2);
+        }
+
     }
 }
