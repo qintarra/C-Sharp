84a30396d4121c672ae8a16279e7572d81a51a18
Qintarra <brain_damage@ukr.net>
2022-06-12 08:51:30 +0300

    ref

--- Notes/Notes_13.cs
+++ Notes/Notes_13.cs
@@ -23,5 +23,28 @@ namespace Notes_13.cs
             Welcome("Jeronimo", StaffPosition.Junior);
             Welcome("Aribeth", StaffPosition.Lead);
         }
+		
+		//13.2
+		static void Method(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= 2;
+                Console.Write($"{arr[i]} ");
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Method (arr); //Method (ref arr); - result will be the same, because... Remember! About reference type of an arrays
+
+            Console.WriteLine();
+            foreach (int x in arr)
+                Console.Write($"{x} ");
+            Console.WriteLine();
+        }
     }
 }
