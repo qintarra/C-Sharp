704bbf7986042fda4d59e71017bab9b718884dda
Qintarra <brain_damage@ukr.net>
2022-06-12 08:49:06 +0300

    enum

---
+++ Notes/Notes_13.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Notes_13.cs
+{
+    enum StaffPosition
+    {
+        Junior,
+        Middle,
+        Senior,
+        Lead
+    }
+    internal class Program
+    {
+        static void Welcome(string name, StaffPosition position)
+        {
+            Console.WriteLine($"Hello, {name}!");
+            Console.WriteLine($"Welcome to the position {position} at Amazon!");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Welcome("Jeronimo", StaffPosition.Junior);
+            Welcome("Aribeth", StaffPosition.Lead);
+        }
+    }
+}
