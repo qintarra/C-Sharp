24607b3ce3357791086a69f13d1696e70d9eb479
Qintarra <brain_damage@ukr.net>
2022-06-12 08:52:55 +0300

    ref

--- Notes/Notes_13.cs
+++ Notes/Notes_13.cs
@@ -25,6 +25,7 @@ namespace Notes_13.cs
         }
 		
 		//13.2
+		
 		static void Method(int[] arr)
         {
             for (int i = 0; i < arr.Length; i++)
@@ -46,5 +47,33 @@ namespace Notes_13.cs
                 Console.Write($"{x} ");
             Console.WriteLine();
         }
+		
+        //13.3
+		
+		static void Method(ref int[] arr) //whith ref
+        {
+            arr = new int[] { 10, 20, 30 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= 2;
+                Console.Write($"{arr[i]} ");
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Method (ref arr);
+
+            Console.WriteLine();
+            foreach (int x in arr)
+                Console.Write($"{x} ");
+            Console.WriteLine();
+
+        }
+        //20 40 60
+        //20 40 60
     }
 }
