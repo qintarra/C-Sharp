b6afd1448dc6b18884c288385fe77730210943b0
Qintarra <brain_damage@ukr.net>
2022-06-13 11:04:42 +0300

    spacing fixed

--- Task_19.cs
+++ Task_19.cs
@@ -35,8 +35,8 @@ namespace Task_19
         * only if the given 'array' is sorted in the given 'order' (the order is set up by enum value SortOrder). 
         * Array and sort order are passed by parameters. To check, if the array is sorted, the function 'IsSorted' from the previous is called.
         * Example: For {5, 17, 24, 88, 33, 2} and “ascending” sort order values in the array do not change.
-        *		   For {15, 10, 3} and “ascending” sort order values in the array do not change.
-        *		   For {15, 10, 3} and “descending” sort order the values in the array change to {15, 11, 5}.
+        *          For {15, 10, 3} and “ascending” sort order values in the array do not change.
+        *          For {15, 10, 3} and “descending” sort order the values in the array change to {15, 11, 5}.
         */
 		
         public static void Transform (int[] array, SortOrder order)
