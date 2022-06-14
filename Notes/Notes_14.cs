91faee2bf1f3b424402b589f3c84182cb1ef80b6
Qintarra <brain_damage@ukr.net>
2022-06-14 11:27:25 +0300

    constructors

--- Notes/Notes_14.cs
+++ Notes/Notes_14.cs
@@ -133,7 +133,44 @@ class Person
 
             Person p3 = new Person();
             p3.AboutMe();
+        }
+    }
 
+//14.4
+class Person
+    {
+        string firstname;
+        string lastname;
+        int age;
 
+        //Constructor with threee parameters
+
+        public Person (string firstname, string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+
+        //Constructor to create 27-years old
+        public Person(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = 27;
+        }
+
+        //Parameterless constructor
+        public Person()
+        {
+            firstname = "Isaak";
+            lastname = "Azimov";
+            age = 70;
+        }
+
+        //Method for obtaining all information about a person
+        public void AboutMe()
+        {
+            Console.WriteLine($"My name is {firstname} {lastname} and I'm {age} years old");
         }
     }
