72f3c4c90ead76a31351de72612485309331641b
Qintarra <brain_damage@ukr.net>
2022-06-14 11:27:38 +0300

    constructors

--- Notes/Notes_14.cs
+++ Notes/Notes_14.cs
@@ -174,3 +174,26 @@ class Person
             Console.WriteLine($"My name is {firstname} {lastname} and I'm {age} years old");
         }
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Douglas", "Adams", 42);
+            p1.AboutMe();
+
+            Person p2 = new Person("James", "Hendrix");
+            p2.AboutMe();
+
+            Person p3 = new Person();
+            p3.AboutMe();
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your lastname");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Person p4 = new Person(name, lastname, age);
+            p4.AboutMe();
+        }
+    }
