0d6513c0d3e0349acf5f32e50b43fdb55db282a3
Qintarra <brain_damage@ukr.net>
2022-06-16 10:46:08 +0300

    class

---
+++ Task_20.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Class
+{
+    //Create public class Rectangle here
+    class Rectangle
+    {
+        //Define two private double fields: 'sideA', 'sideB'
+        private double sideA;
+        private double sideB;
+		
+    }
+    
+}
