e33183b69c3d9a395035113745114be3c14454f6
Qintarra <brain_damage@ukr.net>
2022-06-18 14:40:58 +0300

    class Worker

--- Notes/Notes_14.cs
+++ Notes/Notes_14.cs
@@ -231,3 +231,30 @@
             p4.Info();
         }
     }
+	
+    //14.6
+    class Worker
+    {
+        private string firstname;
+        private string lastname;
+        private int salary;
+        public static int total_salary;
+
+        //Constructor
+        public Worker(string firstname, string lastname, int salary)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.salary = salary;
+            total_salary += salary;
+        }
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Worker w1 = new Worker("Mark", "Zukerberg", 3000000);
+                Worker w2 = new Worker("Bill", "Gates", 2500000);
+                Console.WriteLine($"Total salary {Worker.total_salary}");
+            }
+        }
+    }
