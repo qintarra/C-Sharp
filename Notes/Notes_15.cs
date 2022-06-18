4316db26ce67e0d556a2ac0aac92bddcda38cc9c
Qintarra <brain_damage@ukr.net>
2022-06-18 14:41:49 +0300

    class Person

--- Notes/Notes_15.cs
+++ Notes/Notes_15.cs
@@ -201,3 +201,61 @@
             Console.WriteLine(x); Console.WriteLine(DateTime.FromBinary(x).ToLongTimeString());
         }
     }
+
+    //15.6
+    class Person
+    {
+        private string name;
+        private int number;
+        private static int total;
+        static Person()
+        {
+            total = 1;
+        }
+        public Person (string name)
+        {
+            number = total;
+            this.name = name;
+            total++;
+        }
+        public static int Total
+        {
+            get
+            {
+                return total - 1;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public static void AboutUs()
+        {
+            Console.WriteLine($"We are the people and there are a {Total} of us");
+        }
+        public void AboutMe()
+        {
+            Console.WriteLine($"My name is {name} and I'm object number {number}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person("Tom");
+            Person john = new Person("John");
+            Person ann = new Person("Ann");
+
+            Console.WriteLine($"My name is {tom.Name}");
+
+            tom.AboutMe();
+            john.AboutMe();
+            ann.AboutMe();
+
+            Person.AboutUs();
+            Console.WriteLine(Person.Total);
+        }
+    }
