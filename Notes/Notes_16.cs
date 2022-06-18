c14433eb7a84050e3617ddeaf564bedef52f8844
Qintarra <brain_damage@ukr.net>
2022-06-18 15:00:27 +0300

    virtual method override

--- Notes/Notes_16.cs
+++ Notes/Notes_16.cs
@@ -173,3 +173,49 @@ namespace Notes_16_4
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Notes_16_5
+{
+    class Person
+    {
+        public string firstname;
+        public string lastname;
+        
+        public virtual void AboutMe()
+        {
+            Console.WriteLine($"My name is {firstname} {lastname}.");
+        }
+    }
+    class Student : Person
+    {
+        public string university;
+
+        //Virtual method override
+        public override void AboutMe()
+        {
+            Console.WriteLine($"I'm {firstname} {lastname}, student of {university}.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.firstname = "Mark";
+            p.lastname = "Zukerberg";
+            p.AboutMe();
+
+            Student s = new Student();
+            s.firstname = "Bill";
+            s.lastname = "Gates";
+            s.university = "Harvard";
+            s.AboutMe();
+
+            (s as Person).AboutMe();
+        }
+    }
+}
