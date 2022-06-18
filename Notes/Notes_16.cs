a672af9ee2d6e883b49fceb9c63ddf59f7d70ef2
Qintarra <brain_damage@ukr.net>
2022-06-18 14:54:06 +0300

    Inheritance

--- Notes/Notes_16.cs
+++ Notes/Notes_16.cs
@@ -50,3 +50,42 @@ namespace Notes_16_1
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+namespace Notes_16_2
+{
+    class Person
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+
+        public void AboutMe()
+        {
+            Console.WriteLine($"I'm {firstname} {lastname}. I'm {age} years old.");
+        }
+    }
+    class Student : Person
+    {
+        public string university;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.firstname = "Mark";
+            p.lastname = "Zukerberg";
+            p.age = 17;
+            p.AboutMe();
+            
+            Student s = new Student();
+            s.firstname = "Bill";
+            s.lastname = "Gates";
+            s.age = 22;
+            s.university = "Harvard";
+            s.AboutMe();
+        }
+    }
+}
