84bc0371962e6d681ce1e08dbe7121911c2710a2
Qintarra <brain_damage@ukr.net>
2022-06-19 10:36:29 +0300

    inheritance

--- Notes/Notes_17.cs
+++ Notes/Notes_17.cs
@@ -72,3 +72,59 @@ namespace Notes_17_2
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+namespace Notes_17_3
+{
+    class A
+    {
+        int a;
+        public A()
+        {
+            a = 1;
+        }
+        public void Print()
+        {
+            Console.Write($"a = {a} ");
+        }
+    }
+    class B : A
+    {
+        int b;
+        public B()
+        {
+            b = 2;
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.Write($"b = {b} ");
+        }
+    }
+    class C : B
+    {
+        int c;
+        public C()
+        {
+            c = 3;
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine($"c = {c} ");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            C c = new C();
+            c.Print();
+            (c as B).Print();
+            Console.WriteLine();
+            (c as A).Print();
+            Console.WriteLine();
+        }
+    }
+}
