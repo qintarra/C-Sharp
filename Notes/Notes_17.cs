19822577dc69ca978378c9b6a31cef988c1bf4fd
Qintarra <brain_damage@ukr.net>
2022-06-19 10:33:22 +0300

    instances

---
+++ Notes/Notes_17.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes_17_1
+{
+	class A
+    {
+        public A()
+        {
+            //Instance of Class A created
+            Console.WriteLine("Class A constuctor called");
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            //Instance of Class B created
+            Console.WriteLine("Class B constuctor called");
+        }
+    }
+    class C : B
+    {
+        public C()
+        {
+            //Instance of Class C created
+            Console.WriteLine("Class C constuctor called");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            A a = new A();
+            B b = new B();
+            C c = new C();
+        }
+    }
+}
