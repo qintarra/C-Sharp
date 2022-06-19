d0702dcc2466b0195ba7cf39e78e1a3ac6b97e8b
Qintarra <brain_damage@ukr.net>
2022-06-19 10:27:35 +0300

    'ToString' method override

--- Notes/Notes_16.cs
+++ Notes/Notes_16.cs
@@ -219,3 +219,41 @@ namespace Notes_16_5
         }
     }
 }
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Notes_16_6
+{
+    class Person
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+
+        public override string ToString()
+        {
+            return string.Format($"{firstname} {lastname} {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            double y = 2.5;
+            string s = "Hello!";
+            Person p = new Person();
+            p.firstname = "Bill";
+            p.lastname = "Gates";
+            p.age = 22;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(s);
+            Console.WriteLine(p);
+        }
+    }
+}
