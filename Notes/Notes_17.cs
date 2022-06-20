ec5499f22e00efadad0b93d30cd0158fe59f44c2
Qintarra <brain_damage@ukr.net>
2022-06-20 11:12:12 +0300

    inheritance example

--- Notes/Notes_17.cs
+++ Notes/Notes_17.cs
@@ -213,3 +213,52 @@ namespace Notes_17_4
         }
     }
 }
+
+
+using System;
+
+namespace Notes_17_5
+{
+    class A
+    {
+        public virtual void M()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B : A
+    {
+        public override void M()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class C : B
+    {
+        public new virtual void M()
+        {
+            Console.WriteLine("C");
+        }
+    }
+    class D : C
+    {
+        public override void M()
+        {
+            Console.WriteLine("D");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            D d = new D();
+            C c = d;
+            B b = d;
+            A a = d;
+            d.M();
+            c.M();
+            b.M();
+            a.M();
+        }
+    }
+}
