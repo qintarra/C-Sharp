5e18496d23f6ab759672b81fdfe0875c23282b45
Qintarra <brain_damage@ukr.net>
2022-06-20 11:28:12 +0300

    property 'Name'

--- Task_21-1.cs
+++ Task_21-1.cs
@@ -5,12 +5,12 @@ namespace InheritanceTask
     //Create public class 'Employee' here
     public class Employee
     {
-		//Define three private fields: 'name'(string), 'salary'(decimal), 'bonus'(decimal)
+        //Define three private fields: 'name'(string), 'salary'(decimal), 'bonus'(decimal)
         private readonly string name;
         private decimal salary;
         private decimal bonus;
 		
-		//Define public property 'Name' for getting 'name'
+        //Define public property 'Name' for getting 'name'
         public string Name
         {
             get
@@ -19,40 +19,6 @@ namespace InheritanceTask
             }
         }
 		
-		//Define public property 'Salary' for getting and setting 'salary'
-        public decimal Salary
-        {
-            get
-            {
-                return salary;
-            }
-            set
-            {
-                if (value >= 0)
-                {
-                    salary = value;
-                }
-            }
-        }
-		
-		//Define constructor with two parameters: 'name'(string), 'salary'(decimal). Assign parameters to its fields
-        public Employee (string name, decimal salary)
-        {
-            this.name = name;
-            this.salary = salary;
-        }
-		
-		//Define public virtual method 'SetBonus', that set value in 'bonus'
-        public virtual void SetBonus (decimal bonus)
-        {
-            this.bonus = bonus;
-        }
-		
-		//Define public method 'ToPay', that returns sum of salary and bonus 
-        public decimal ToPay()
-        {
-            return salary + bonus;
-        }
     }
 }
 
