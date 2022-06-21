49efb433f68d40b28e00ba79fd2c2f757df72590
Qintarra <brain_damage@ukr.net>
2022-06-21 08:50:28 +0300

    SalesPerson added

---
+++ Task_21-2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_21_2
+{
+    //Create public class 'SalesPerson' here, which inherits from the 'Employee' class
+    public class SalesPerson : Employee
+    {
+        
+    }
+}
