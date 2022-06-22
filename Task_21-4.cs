8fb2afed0f366a555bbb6040b4d059a896ccbb65
Qintarra <brain_damage@ukr.net>
2022-06-22 10:00:51 +0300

    'Company' class

---
+++ Task_21-4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Task_21_4
+{
+    //Create public class 'Company' here.
+    public class Company
+    {
+        
+    }
+}
