4894edfc22872f11cb966e8f6e2aba55a5beaa96
Qintarra <brain_damage@ukr.net>
2022-06-22 09:50:09 +0300

    'Employee' class

---
+++ Task_21-3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_21_3
+{
+    //Create public class 'Manager' here, which inherits from the 'Employee' class.
+    public class Manager : Employee
+    {
+       
+    }
+}
+
