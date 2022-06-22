06e612aab584e238378adc2cfb157531e4682c2c
Qintarra <brain_damage@ukr.net>
2022-06-22 09:52:04 +0300

    'quantity' field

--- Task_21-3.cs
+++ Task_21-3.cs
@@ -7,7 +7,10 @@ namespace Task_21_3
     //Create public class 'Manager' here, which inherits from the 'Employee' class.
     public class Manager : Employee
     {
-       
+        //Define closed integer field: 'quantity' (number of clients, who were served by the manager during a month).
+        private readonly int quantity;
+		
+        
     }
 }
 
