0704d9f8442a7927b363a361a4be6f7c069994b9
Qintarra <brain_damage@ukr.net>
2022-07-05 18:05:01 +0300

    method

---
+++ Notes/04_Methods/03_M.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Method_0
+{
+    class Program
+    {
+
+		static void Hello(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+        static void Main()
+        {
+              Hello("Andy");
+              Hello("Maria");
+        }
+    }
+}
