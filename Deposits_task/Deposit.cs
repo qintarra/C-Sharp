7fc7651eca5351134ab79b4428599e31be4dfe63
Qintarra <brain_damage@ukr.net>
2022-07-14 13:25:13 +0300

    comments updated

--- Deposits_task/Deposit.cs
+++ Deposits_task/Deposit.cs
@@ -1,22 +1,22 @@
 namespace Aggregation
 {
-	//Define public abstract class "Deposit"
+    //Define public abstract class "Deposit"
     public abstract class Deposit
     {
-		//Define public readonly property "Amount" with decimal type.
+        //Define public readonly property "Amount" with decimal type.
         public decimal Amount { get; }
 		
-		//Define public readonly property "Period" with int type.
+        //Define public readonly property "Period" with int type.
         public int Period { get; }
 		
-		//Define constructor that gets "Amount" and "Period" and assigns them to its properties.
+        //Define constructor that gets "Amount" and "Period" and assigns them to its properties.
         public Deposit(decimal amount, int period)
         {
             Amount = amount;
             Period = period;
         }
 		
-		//Define public abstract method "Income" that returns deposit profit depending on "Amount" and "Period".
+        //Define public abstract method "Income" that returns deposit profit depending on "Amount" and "Period".
         public abstract decimal Income();
     }
 }
