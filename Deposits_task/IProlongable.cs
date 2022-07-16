1474f20ea9daeb52253618db236e21da7413962f
Qintarra <brain_damage@ukr.net>
2022-07-16 11:47:54 +0300

    comments added

--- Deposits_task/IProlongable.cs
+++ Deposits_task/IProlongable.cs
@@ -4,8 +4,10 @@ using System.Text;
 
 namespace Interface
 {
+	//Create interface: "IProlongable".
     interface IProlongable
     {
+		//Create a method "CanToProlong" signature that returns true or false.
         bool CanToProlong();
     }
 }
