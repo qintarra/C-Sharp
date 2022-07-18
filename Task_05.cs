2dec3fa7cf205501b133fd8eb2984a9c97a214c3
Qintarra <brain_damage@ukr.net>
2022-07-18 11:35:48 +0300

    comments improved

--- Task_05.cs
+++ Task_05.cs
@@ -2,13 +2,10 @@
 {
     public static class Tasks
     {
-        public static int Task1(int a)
-
         /* Given an integer A. If A is positive, then add 1 to it. Otherwise subtract 2 from it.  
-         * Example: A = 19  =>  A = 20  
-         *          A = -13  =>  A = -15
-         */
-
+        Example: A = 19  =>  A = 20  
+                 A = -13  =>  A = -15 */
+        public static int Task1(int a)     
         {
             if (a > 0)
             {
@@ -18,18 +15,14 @@
             {
                 return a - 2;
             }
-		// Another option: a = a > 0 ? a + 1 : a - 2;
+        // Another option: a = a > 0 ? a + 1 : a - 2;
         }
 		
+        /* Given an integer A. If it is positive, then add 4 to it; if negative, then subtract 7 from it; if zero, then replace it with 5.
+        Example: A = 42  =>  A = 46
+                 A = -14  =>  A = -21
+                 A = 0  =>  A = 5 */
         public static int Task2(int a)
-
-        /* Given an integer A. If it is positive, then add 4 to it; 
-         * if negative, then subtract 7 from it; if zero, then replace it with 5.
-         * Example: A = 42  =>  A = 46
-         *          A = -14  =>  A = -21
-         *          A = 0  =>  A = 5
-         */
-
         {
             if (a > 0)
             {
@@ -46,17 +39,14 @@
                     return 5;
                 }
             }
-		// Another option: a = a > 0 ? a + 4 : a < 0 ? a - 7 : a = 5;
+        // Another option: a = a > 0 ? a + 4 : a < 0 ? a - 7 : a = 5;
         }
 		
-        public static (int a, int b) Task3(int a, int b)
-
         /* Two values of integer type are given: A and B. If their values are not equal, then assign the sum of these values to each variable, 
-         * and if they are equal, then assign zero values to the variables.
-         * Example: A = 5;  B = 5 =>  A = 0; B = 0
-         *          A = -25;  B = 8 =>  A = -17; B = -17
-         */
-
+        and if they are equal, then assign zero values to the variables.
+        Example: A = 5;  B = 5 =>  A = 0; B = 0
+                 A = -25;  B = 8 =>  A = -17; B = -17 */
+        public static (int a, int b) Task3(int a, int b)
         {
             if (a != b)
             {
@@ -68,13 +58,10 @@
             }
         }
 
-        public static int Task4(int a, int b, int c)
-
         /* Given three unequal numbers A, B, C. Find the smallest of them.
-         * Example: A = 8;  B = -2 ; C = 3 =>  Min = -2
-         *          A = -24;  B = -3 ; C = 0 =>  Min = -24
-         */
-
+        Example: A = 8;  B = -2 ; C = 3 =>  Min = -2
+                 A = -24;  B = -3 ; C = 0 =>  Min = -24 */
+        public static int Task4(int a, int b, int c)
         {
             var min = Math.Min(a, b);
             if (min < c)
@@ -86,21 +73,15 @@
                 return c;
             }
         }
-		
-        public static int Task5(int n)
-		
+			
         /* For a given integer n calculate the value which is equal to:
-        *
-        *squared number, if its value is strictly positive;
-        *modulus of a number, if its value is strictly negative;
-        *zero, if the integer n is zero.
-        *
-        *Example:
-        *		n = 4     result =  16 
-        *		n = -5    result =  5 
-        *		n = 0     result = 0 
-        */
-        
+        squared number, if its value is strictly positive;
+        modulus of a number, if its value is strictly negative;
+        zero, if the integer n is zero.
+        Example: n = 4     result =  16       		
+        		 n = -5    result =  5 
+        		 n = 0     result = 0 */
+        public static int Task5(int n)        
         {
             if (n > 0)
             {
