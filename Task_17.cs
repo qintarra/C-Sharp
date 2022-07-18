61c9f08b9cb41d04940b82c4641fde14d7425ca5
Qintarra <brain_damage@ukr.net>
2022-07-18 12:17:34 +0300

    task description corrected

--- Task_17.cs
+++ Task_17.cs
@@ -4,9 +4,13 @@ namespace ArrayObject
 {
     public static class ArrayTasks
     {
-        //In a given array of integers 'nums' swap values of the first and the last array elements, the second and the penultimate, etc., 
-        //if the two exchanged values are even
-        //Example: {100, 2, 3, 45, 33, 8, 4, 54} => {54, 4, 3, 45, 33, 8, 2, 100}
+        /* In a given array of integers 'nums' swap values of the first and the last array elements, the second and the penultimate, etc., 
+        if the two exchanged values are even
+        Example: 
+                 { 10 , 5, 3, 4}	           =>  {4, 5, 3, 10} 
+                 {100, 2, 3, 4, 5}             => {100, 4, 3, 2, 5} 
+                 {100, 2, 3, 45, 33, 8, 4, 54} => {54, 4, 3, 45, 33, 8, 2, 100} 
+		*/
         public static void ChangeElementsInArray(int[] nums)
         {
             int x;
@@ -22,13 +26,12 @@ namespace ArrayObject
         }
 		
        /* In a given array of integers 'nums' calculate the integer result value, 
-        * which is equal to the distance between the first and the last entry of the maximum value in the array.
-        * Example: 
-        *			{4, 100!, 3, 4}         result = 0 
-        *			{5, 50!, 50!, 4, 5}     result = 1 
-        *			{5, 350!, 350, 4, 350!} result = 3 
+        which is equal to the distance between the first and the last entry of the maximum value in the array.
+        Example: 
+                 {4, 100!, 3, 4}         result = 0 
+                 {5, 50!, 50!, 4, 5}     result = 1 
+                 {5, 350!, 350, 4, 350!} result = 3 
         */
-
         public static int DistanceBetweenFirstAndLastOccurrenceOfMaxValue(int[] nums)
         {
             int maxFirst = 0;
@@ -49,13 +52,13 @@ namespace ArrayObject
         }
 		
        /* In a predetermined two-dimensional integer array (square matrix) matrix insert 0 into elements to the left side of the main diagonal, 
-        * and 1 into elements to the right side of the diagonal.
-        * Example: {{2, 4, 3, 3},          {{2, 1, 1, 1},
-        *			{5, 7, 8, 5},    =>     {0, 7, 1, 1},          
-        *			{2, 4, 3, 3},           {0, 0, 3, 1},
-        *			{5, 7, 8, 5}}           {0, 0, 0, 5}}
-        */
-		
+        and 1 into elements to the right side of the diagonal.
+        Example: 
+                 {{2, 4, 3, 3},          {{2, 1, 1, 1},
+                 {5, 7, 8, 5},    =>     {0, 7, 1, 1},          
+                 {2, 4, 3, 3},           {0, 0, 3, 1},
+                 {5, 7, 8, 5}}           {0, 0, 0, 5}}
+        */		
         public static void ChangeMatrixDiagonally(Array matrix)
         {
             for (int i = 1; i < matrix.GetLength(0); i++)
