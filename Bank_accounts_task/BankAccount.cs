3d6c5d4d03280c3b56d598e75ae592a7e2d85895
Qintarra <brain_damage@ukr.net>
2022-07-20 14:42:13 +0300

    bank accounts added

---
+++ Bank_accounts_task/BankAccount.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ProgChallenge
+{
+    public class BankAccount
+    {
+        private readonly string _firstname;
+        private readonly string _lastname;
+
+        public decimal Balance { get; set; }
+
+        public BankAccount(string fname, string lname, decimal initial = 0.0m)
+        {
+            _firstname = fname;
+            _lastname = lname;
+            Balance = initial;
+        }
+
+        public string AccountOwner
+        {
+            get => $"{_firstname} {_lastname}";
+        }
+
+        public virtual void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public virtual void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
