c25b28e1f38fa242a34ce15e45a045edb93e9b3d
Qintarra <brain_damage@ukr.net>
2022-07-20 14:43:02 +0300

    checking

---
+++ Bank_accounts_task/CheckingAccount.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ProgChallenge
+{
+    public class CheckingAccount : BankAccount
+    {
+        private const decimal OVERDRAW_CHARGE = 35.0m;
+
+        public CheckingAccount(string fname, string lname, decimal initial) : base(fname, lname, initial)
+        {
+
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            // if the account gets overdrawn add an extra charge
+            if (amount > Balance)
+            {
+                amount += OVERDRAW_CHARGE;
+            }
+            base.Withdraw(amount);
+        }
+    }
+}
