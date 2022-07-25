1a87eba13104b33ddce5373d93cb47850a376116
Qintarra <brain_damage@ukr.net>
2022-07-25 09:50:34 +0300

    tuples

---
+++ Notes/10_Collections/20_Tuple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningTuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tuple<int, string, bool> myTuple = new Tuple<int, string, bool>(1, "hello", true);
+
+            var myTuple = Tuple.Create(1, "hello", true);
+
+            Console.WriteLine(myTuple.Item2);
+        }
+    }
+}
