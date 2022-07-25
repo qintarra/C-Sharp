43bea571c904d7a02515b68d7510242affc9e2bf
Qintarra <brain_damage@ukr.net>
2022-07-25 09:51:01 +0300

    HashSet

---
+++ Notes/10_Collections/21_HashSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHash = new HashSet<string>();
+
+            myHash.Add("hello");
+            myHash.Add("hello");
+
+            string[] s = new string[] { "hello" };
+
+            Console.WriteLine(myHash.Count);
+            Console.WriteLine(myHash.Overlaps(s));
+        }
+    }
+}
