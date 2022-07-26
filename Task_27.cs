b75f11719c65cf028f2011ecfebf8c311ae34ee2
Qintarra <brain_damage@ukr.net>
2022-07-26 09:35:12 +0300

    generic

---
+++ Task_27.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsTask
+{
+    public class ClassicCar
+    {
+        public string m_Make;
+        public string m_Model;
+        public int m_Year;
+        public int m_Value;
+
+        public ClassicCar(string make, string model, int year, int val)
+        {
+            m_Make = make;
+            m_Model = model;
+            m_Year = year;
+            m_Value = val;
+        }
+    }
+
+    
+}
