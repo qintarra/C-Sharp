666b9459e6c9eb1212922e5b363e4d42999ce662
Qintarra <brain_damage@ukr.net>
2022-07-27 08:54:06 +0300

    comments fixed

--- Tasks/Task_04.cs
+++ Tasks/Task_04.cs
@@ -2,14 +2,12 @@
 {
     public static class Tasks
     {   
+	    /* The rectangle's sides are a and b. Find its perimeter using the formula: P = 2×(a + b).
+        Find its area following the formula: S = a × b
+        Example: а = 5, b = 3   perimeter = 16
+                 а = 5, b = 3   area = 15 */
+         
         public static void Task1(int a, int b)
-
-        /* The rectangle's sides are a and b. Find its perimeter using the formula: P = 2×(a + b).
-         * Find its area following the formula: S = a × b
-         * Example: а = 5, b = 3   perimeter = 16
-         *          а = 5, b = 3   area = 15
-         */
-
         {
             int perimeter = 2 * (a + b), area = a * b;
 
