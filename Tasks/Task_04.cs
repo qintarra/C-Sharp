9d318ae9d6f66675625f635bfe3dd164e28f4231
Qintarra <brain_damage@ukr.net>
2022-07-28 09:14:58 +0300

    update

--- Tasks/Task_04.cs
+++ Tasks/Task_04.cs
@@ -4,9 +4,7 @@
     {   
 	    /* The rectangle's sides are a and b. Find its perimeter using the formula: P = 2×(a + b).
         Find its area following the formula: S = a × b
-        Example: а = 5, b = 3   perimeter = 16
-                 а = 5, b = 3   area = 15 */
-         
+        Example: а = 5, b = 3,	 perimeter = 16,	 area = 15 */      
         public static void Task1(int a, int b)
         {
             int perimeter = 2 * (a + b), area = a * b;
@@ -14,13 +12,10 @@
             Console.WriteLine($"P = {perimeter}, S = {area}");
         }
 
-        public static void Task2(int a)
-
         /* The cube`s side is a. Find its volume using the formula: V = a³.
-         * Find its surface area following the formula: S = 6 × a²
-         * Example: а = 5   volume = 125    surface area = 150
-         */
-
+        Find its surface area following the formula: S = 6 × a²
+        Example: а = 5,   volume = 125,    surface area = 150 */    
+        public static void Task2(int a)
         {
             int volume = a * a * a, surfaceArea = 6 * a * a;
 
