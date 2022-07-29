e7b69613728c96a1ad993d7daffc45bc3c719d8b
Qintarra <brain_damage@ukr.net>
2022-07-29 10:41:50 +0300

    update

--- Tasks/Task_04.cs
+++ Tasks/Task_04.cs
@@ -41,18 +41,15 @@
 
             Console.WriteLine($"V = {volume}, S = {surfaceArea}");
         }
-
+		
+        /* The speed of the boat in the stagnant water is V km/h, and the speed of the river's flow is U km/h (U < V). 
+        The boat's moving time in the lake is T1 h and in the river (against the flow) is T2 h. 
+        Find the distance, that was passed by a boat (distance = speed * time). 
+        Take into consideration that when the boat is moving against the flow, the boat`s speed decreases with the speed of flow.
+        Example: V = 20 km/h; U = 5 km/h; T1 = 2 h; T2 = 3 h  =>  Distance = 85 km 
+                 V = 18.7 km/h; U = 3.2 km/h; T1 = 0.5 h; T2 = 1.25 h  =>  Distance = 28.73 km 
+                 V = 22 km/h; U = 7.3 km/h; T1 = 2 h; T2 = 1.5 h  =>  Distance = 66.05 km */      
         public static void Task5(double v, double u, double t1, double t2)
-
-        /* The speed of the boat in the stagnant water is V km/h, the speed of the river's flow is U km/h (U < V). 
-         * The boat's moving time in the lake is T1 h and in the river (against the flow) is T2 h. 
-         * Find  distance, that was passed by a boat (distance = speed * time). 
-         * Take into consideration that when the boat is moving against the flow, the boat`s speed decreases by the speed of flow.
-         * Example: V = 20 km/h; U = 5 km/h; T1 = 2 h; T2 = 3 h  =>  Distance = 85 km 
-         *          V = 18.7 km/h; U = 3.2 km/h; T1 = 0.5 h; T2 = 1.25 h  =>  Distance = 28.73 km 
-         *          V = 22 km/h; U = 7.3 km/h; T1 = 2 h; T2 = 1.5 h  =>  Distance = 66.05 km 
-         */
-
         {
             double distance = v * t1 + (v - u) * t2;
 
@@ -61,9 +58,9 @@
 
         public static void Task6(double s, double t, double v1, double v2)
 
-        /* The speed of the first car is V1 km/h, the speed of the second car is V2 km/h, the distance between them is S km. 
+        /* The speed of the first car is V1 km/h, the speed of the second car is V2 km/h, and the distance between them is S km. 
          * Find the distance between cars after T hours, if the cars are distancing from each other. 
-         * The given distance is equal the sum of the initial distance + the general way, which was covered by cars; the general way = time*total speed.
+         * The given distance is equal to the sum of the initial distance + the general way, which was covered by cars; the general way = time*total speed.
          * Example: V1 = 90 km/h; V2 = 110 km/h; S = 65 km; T = 3 h  =>  Distance = 665 km 
          *          V1 = 65.5 km/h; V2 = 90.4 km/h; S = 20.9 km; T = 1.5 h  =>  Distance = 254.75 km 
          *          V1 = 70 km/h; V2 = 85.6 km/h; S = 32.6 km; T = 2 h  =>  Distance = 343.8 km  
