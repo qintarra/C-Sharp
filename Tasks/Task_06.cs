b1190d07e4543d8c019b8716cd64372f70370160
Qintarra <brain_damage@ukr.net>
2022-07-29 10:50:29 +0300

    t1

--- Tasks/Task_06.cs
+++ Tasks/Task_06.cs
@@ -5,11 +5,9 @@
         public static int Task1(int a,  int b, int c)
 
         /* Given three unequal numbers A, B, C. Find the sum of the two largest of them
-         * Example: A = 11;  B = 34 ; C = 7 =>  Result = 45
-         *          A = 17;  B = -12 ; C = -6 =>  Result = 11
-         *          A = 5;  B = 0 ; C = 13 =>  Result = 18
-         */     
-
+        Example: A = 11;  B = 34 ; C = 7 =>  Result = 45
+                 A = 17;  B = -12 ; C = -6 =>  Result = 11
+                 A = 5;  B = 0 ; C = 13 =>  Result = 18 */
         {
             var result = 0;
             if (a > b && b > c)
