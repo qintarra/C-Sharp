c177d4538cc415f9a56f61a7921675fe49e28a22
Qintarra <brain_damage@ukr.net>
2022-07-29 10:52:13 +0300

    t2

--- Tasks/Task_06.cs
+++ Tasks/Task_06.cs
@@ -28,14 +28,13 @@
             return result;
         }
 
-        public static (int a, int b, int c) Task2(int a, int b, int c)
+
 
         /* Given three real type values A, B, C. Rewrite the values of the variables so that A has the smaller of the values, and C has the larger one.
-         * Example: A = 10;  B = -15 ; C = 7 =>  A = -15;  B = -15 ; C = 10
-         *          A = -1;  B = 10 ; C = 5 =>  A = -1;  B = 10 ; C = 10
-         *          A = -17;  B = -2 ; C = -14 =>  A = -17;  B = -2 ; C = -2
-         */
-
+        Example: A = 10;  B = -15 ; C = 7 =>  A = -15;  B = -15 ; C = 10
+                 A = -1;  B = 10 ; C = 5 =>  A = -1;  B = 10 ; C = 10
+                 A = -17;  B = -2 ; C = -14 =>  A = -17;  B = -2 ; C = -2 */       
+        public static (int a, int b, int c) Task2(int a, int b, int c)
         {
             int min = Math.Min(Math.Min(a, b), c);
             int max = Math.Max(Math.Max(a, b), c);
