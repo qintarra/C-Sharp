6f7cdc96afbf3cff9573135c9b758ef5a33ddfab
Qintarra <brain_damage@ukr.net>
2022-08-03 09:16:47 +0300

    transform

--- Tasks/Task_19.cs
+++ Tasks/Task_19.cs
@@ -31,13 +31,11 @@ namespace Task_19
         }
 		
        /* Create function 'Transform', replacing the value of each element of an integer 'array' with the sum of this element value and its index, 
-        * only if the given 'array' is sorted in the given 'order' (the order is set up by enum value SortOrder). 
-        * Array and sort order are passed by parameters. To check, if the array is sorted, the function 'IsSorted' from the previous is called.
-        * Example: For {5, 17, 24, 88, 33, 2} and “ascending” sort order values in the array do not change.
-        *          For {15, 10, 3} and “ascending” sort order values in the array do not change.
-        *          For {15, 10, 3} and “descending” sort order the values in the array change to {15, 11, 5}.
-        */
-		
+        only if the given 'array' is sorted in the given 'order' (the order is set up by enum value SortOrder). 
+        Array and sort order are passed by parameters. To check, if the array is sorted, the function 'IsSorted' from the previous is called.
+        Example: For {5, 17, 24, 88, 33, 2} and “ascending” sort order values in the array do not change.
+                 For {15, 10, 3} and “ascending” sort order values in the array do not change.
+                 For {15, 10, 3} and “descending” sort order the values in the array change to {15, 11, 5}. */      		
         public static void Transform (int[] array, SortOrder order)
         {
             if (IsSorted(array, order))
