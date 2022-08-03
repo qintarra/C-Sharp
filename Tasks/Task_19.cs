d0aba779a0ee9de9d881c94109641536e89d154f
Qintarra <brain_damage@ukr.net>
2022-08-03 09:15:41 +0300

    upd

--- Tasks/Task_19.cs
+++ Tasks/Task_19.cs
@@ -6,9 +6,8 @@ namespace Task_19
     public static class Function
     {
        /* Create function 'IsSorted', determining whether a given 'array' of integer values of arbitrary length 
-        * is sorted in a given 'order' (the order is set up by enum value 'SortOrder'). 
-        * Array and sort order are passed by parameters. The function does not change the array.
-        */
+        is sorted in a given 'order' (the order is set up by enum value 'SortOrder'). 
+        Array and sort order are passed by parameters. The function does not change the array. */      
         public static Boolean IsSorted (int [] array, SortOrder order)
         {
             switch (order)
