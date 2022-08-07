78694842ada056102441a13debf949b07f7b3448
Qintarra <brain_damage@ukr.net>
2022-08-07 10:13:18 +0300

    interface added

--- Tasks/Deposits_task/Deposit.cs
+++ Tasks/Deposits_task/Deposit.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace Deposit
 {
     //Define public abstract class "Deposit"
--- Tasks/Deposits_task/LongDeposit.cs
+++ Tasks/Deposits_task/LongDeposit.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace Deposit
 {
     //Define public class "LongDeposit" that inherits from "Deposit".
