864c515ba71c923f63a2c19550ab4cb8e1d4c3ae
Qintarra <brain_damage@ukr.net>
2022-08-08 12:52:18 +0300

    array length

--- Tasks/Collections_task/CustomArray.cs
+++ Tasks/Collections_task/CustomArray.cs
@@ -27,6 +27,22 @@ namespace CustomArray
             }
         }
 
+        /// <summary>
+        /// Should return length of array
+        /// <exception cref="ArgumentException">Thrown when value was smaller than 0</exception>
+        /// </summary>
+        public int Length
+        {
+            get
+            {
+                return Array.Length;
+            }
+            private set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Value is smaller than 0");
+            }
+        }
 
         
 
