dfeebcc5d08e0b96f6d5c69e44cf5d8b9a541336
Qintarra <brain_damage@ukr.net>
2022-11-10 11:11:46 +0200

    task example

---
+++ Tasks/LINQ_task/Linq_tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Linq.Objects;
+
+namespace Linq
+{
+    public static class Tasks
+    {
+        public static string TaskExample(IEnumerable<string> stringList)
+        {
+            return stringList.Aggregate<string>((x, y) => x + y);
+        }
+
+    }
+}
