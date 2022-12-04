4a695b8047f3d5ba31218183e92a038ef4201bf7
qintarra <96946215+qintarra@users.noreply.github.com>
2022-12-04 10:55:28 +0200

    cart manager

---
+++ Tasks/HPlusSport_task/02/HPlusSportTDD.Core.Tests/ShoppingCartManager.cs
@@ -0,0 +1,36 @@
+﻿namespace HPlusSportTDD.Core
+{
+    public class ShoppingCartManager : IShoppingCartManager
+    {
+        private readonly List<AddToCartItem> _shoppingCart;
+
+        public ShoppingCartManager()
+        {
+            _shoppingCart = new List<AddToCartItem>();
+        }
+
+        public AddToCartResponse AddToCart(AddToCartRequest request)
+        {
+            var item = _shoppingCart.Find(i => i.ArticleId == request.Item.ArticleId);
+            if (item != null)
+            {
+                item.Quantity += request.Item.Quantity;
+            }
+            else
+            {
+                _shoppingCart.Add(request.Item);
+            }
+            _shoppingCart.Add(request.Item);
+
+            return new AddToCartResponse()
+            {
+                Items = _shoppingCart.ToArray()
+            };
+        }
+
+        public AddToCartItem[] GetCart()
+        {
+            return _shoppingCart.ToArray();
+        }
+    }
+}
