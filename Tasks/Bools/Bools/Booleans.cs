818372c070f2c165e69a1741a1d9576c35c77d30
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-01 11:53:19 +0200

    returns

--- Tasks/Bools/Bools/Booleans.cs
+++ Tasks/Bools/Bools/Booleans.cs
@@ -5,13 +5,13 @@
         public static bool ReturnTrue()
         {
             // Return "true" literal.
-            
+            return true;
         }
 
         public static bool ReturnFalse()
         {
             // Return "false" literal.
-            
+            return false;
         }
     }
 }
