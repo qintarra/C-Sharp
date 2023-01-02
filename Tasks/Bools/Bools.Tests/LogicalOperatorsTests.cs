7896ac8cf733c601987c41d8b81dae0f879d0781
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-02 10:58:15 +0200

    LogicalXor2_ReturnBool1

--- Tasks/Bools/Bools.Tests/LogicalOperatorsTests.cs
+++ Tasks/Bools/Bools.Tests/LogicalOperatorsTests.cs
@@ -98,6 +98,19 @@
             return LogicalOperators.LogicalXor1(b1, b2);
         }
 
+        [TestCase(false, false, false, ExpectedResult = false)]
+        [TestCase(true, false, false, ExpectedResult = true)]
+        [TestCase(false, true, false, ExpectedResult = true)]
+        [TestCase(false, false, true, ExpectedResult = true)]
+        [TestCase(true, true, false, ExpectedResult = false)]
+        [TestCase(true, false, true, ExpectedResult = false)]
+        [TestCase(false, true, true, ExpectedResult = false)]
+        [TestCase(true, true, true, ExpectedResult = true)]
+        public static bool LogicalXor2_ReturnBool(bool b1, bool b2, bool b3)
+        {
+            return LogicalOperators.LogicalXor2(b1, b2, b3);
+        }
+
 
 
     }
