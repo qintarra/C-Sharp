4a83feb7fed7a00d1bb7d4234047b821f65c6b2f
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-02 10:55:03 +0200

    LogicalAnd1_ReturnBool1

---
+++ Tasks/Bools/Bools.Tests/LogicalOperatorsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Bools.Tests
+{
+    [TestFixture]
+    public class LogicalOperatorsTests
+    {
+        [TestCase(false, false, ExpectedResult = false)]
+        [TestCase(true, false, ExpectedResult = false)]
+        [TestCase(false, true, ExpectedResult = false)]
+        [TestCase(true, true, ExpectedResult = true)]
+        public static bool LogicalAnd1_ReturnBool(bool b1, bool b2)
+        {
+            return LogicalOperators.LogicalAnd1(b1, b2);
+        }
+
+
+
+    }
+}
