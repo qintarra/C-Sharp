ec500c5e50d4f8af6b3cfd1e0463b69de0c1c9a5
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-02 10:55:53 +0200

    ReturnBool 2

--- Tasks/Bools/Bools.Tests/LogicalOperatorsTests.cs
+++ Tasks/Bools/Bools.Tests/LogicalOperatorsTests.cs
@@ -12,6 +12,19 @@
             return LogicalOperators.LogicalAnd1(b1, b2);
         }
 
+        [TestCase(false, false, false, ExpectedResult = false)]
+        [TestCase(true, false, false, ExpectedResult = false)]
+        [TestCase(false, true, false, ExpectedResult = false)]
+        [TestCase(false, false, true, ExpectedResult = false)]
+        [TestCase(true, true, false, ExpectedResult = false)]
+        [TestCase(true, false, true, ExpectedResult = false)]
+        [TestCase(false, true, true, ExpectedResult = false)]
+        [TestCase(true, true, true, ExpectedResult = true)]
+        public static bool LogicalAnd2_ReturnBool(bool b1, bool b2, bool b3)
+        {
+            return LogicalOperators.LogicalAnd2(b1, b2, b3);
+        }
+
 
 
     }
