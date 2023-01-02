b3dfd3fd5338694ffd021a0b665734777dff4192
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-03 00:06:06 +0200

    and 1

---
+++ Tasks/Bools/Bools/LogicalOperators.cs
@@ -0,0 +1,12 @@
+﻿namespace Bools
+{
+    public static class LogicalOperators
+    {
+        public static bool LogicalAnd1(bool b1, bool b2)
+        {
+            // Return the result of logical AND for b1 and b2 parameters.
+            return b1 && b2;
+        }
+
+    }
+}
