81e85374ad5a7b3d8f2cae9eebcc6d5cbcb29276
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-03 00:06:31 +0200

    and 3

--- Tasks/Bools/Bools/LogicalOperators.cs
+++ Tasks/Bools/Bools/LogicalOperators.cs
@@ -14,5 +14,11 @@
             return b1 && b2 && b3;
         }
 
+        public static bool LogicalAnd3(bool b1, bool b2, bool b3, bool b4)
+        {
+            // Return the result of logical AND for b1, b2, b3 and b4 parameters.
+            return b1 && b2 && b3 && b4;
+        }
+
     }
 }
