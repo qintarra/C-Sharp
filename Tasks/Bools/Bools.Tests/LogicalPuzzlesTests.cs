e6b3a380930f0475d1e709ae237eafc276d75810
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-04 11:21:42 +0200

    puzzle 4

--- Tasks/Bools/Bools.Tests/LogicalPuzzlesTests.cs
+++ Tasks/Bools/Bools.Tests/LogicalPuzzlesTests.cs
@@ -21,5 +21,23 @@
             return LogicalPuzzles.Puzzle2(b1, b2);
         }
 
+        [TestCase(false, false, ExpectedResult = true)]
+        [TestCase(true, false, ExpectedResult = true)]
+        [TestCase(false, true, ExpectedResult = false)]
+        [TestCase(true, true, ExpectedResult = true)]
+        public bool Puzzle3_ReturnBool(bool b1, bool b2)
+        {
+            return LogicalPuzzles.Puzzle3(b1, b2);
+        }
+
+        [TestCase(false, false, ExpectedResult = false)]
+        [TestCase(true, false, ExpectedResult = true)]
+        [TestCase(false, true, ExpectedResult = false)]
+        [TestCase(true, true, ExpectedResult = false)]
+        public bool Puzzle4_ReturnBool(bool b1, bool b2)
+        {
+            return LogicalPuzzles.Puzzle4(b1, b2);
+        }
+
     }
 }
