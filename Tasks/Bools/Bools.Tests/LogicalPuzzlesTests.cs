e55c2811757c6daf9e2ed057fb1f8ffa82988ff2
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-04 11:21:18 +0200

    puzzle 1

---
+++ Tasks/Bools/Bools.Tests/LogicalPuzzlesTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Bools.Tests
+{
+    [TestFixture]
+    public class LogicalPuzzlesTests
+    {
+        [TestCase(false, false, ExpectedResult = true)]
+        [TestCase(true, false, ExpectedResult = false)]
+        [TestCase(false, true, ExpectedResult = true)]
+        [TestCase(true, true, ExpectedResult = true)]
+        public bool Puzzle1_ReturnBool(bool b1, bool b2)
+        {
+            return LogicalPuzzles.Puzzle1(b1, b2);
+        }
+
+    }
+}
