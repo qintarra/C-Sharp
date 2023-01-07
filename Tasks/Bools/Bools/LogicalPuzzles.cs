376749bcfd25d46d568e88c126f4d8cd28185244
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-07 10:42:25 +0200

    puzzles

---
+++ Tasks/Bools/Bools/LogicalPuzzles.cs
@@ -0,0 +1,9 @@
+﻿namespace Bools
+{
+    // Take a look at the list of test cases.
+    // Solve this puzzles by using logical operators only.
+    public static class LogicalPuzzles
+    {
+
+    }
+}
