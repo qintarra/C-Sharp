9ab0e2e7298d613e6dbd91d3d89d4368de2ab608
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-09 10:32:17 +0200

    task completed

--- Tasks/Leap_year/LeapYear/Year.cs
+++ Tasks/Leap_year/LeapYear/Year.cs
@@ -9,7 +9,7 @@
         /// <returns>True if a specified <paramref name="year"/> is a leap year; otherwise false.</returns>
         public static bool IsLeapYear(int year)
         {
-            
+            return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
         }
     }
 }
