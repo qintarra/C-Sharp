940cdbaf43e79d31fa2c8118cc772c31228afaa1
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-09 10:31:32 +0200

    leap year task

---
+++ Tasks/Leap_year/LeapYear/Year.cs
@@ -0,0 +1,15 @@
+﻿namespace LeapYearTask
+{
+    public static class Year
+    {
+        /// <summary>
+        /// Returns true if a specified <paramref name="year"/> is a leap year.
+        /// </summary>
+        /// <param name="year">A year to test.</param>
+        /// <returns>True if a specified <paramref name="year"/> is a leap year; otherwise false.</returns>
+        public static bool IsLeapYear(int year)
+        {
+            
+        }
+    }
+}
