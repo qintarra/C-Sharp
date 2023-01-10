bcd94992e9e99c7ae966fa97cae21520c9d744b2
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-10 10:11:14 +0200

    tests

---
+++ Tasks/Leap_year/LeapYear.Tests/YearTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeapYearTask.Tests
+{
+    [TestFixture]
+    public class YearTests
+    {
+
+        public void IsLeapYear_ReturnFalse(int year)
+        {
+            Assert.False(Year.IsLeapYear(year));
+        }
+
+
+        public void IsLeapYear_ReturnTrue(int year)
+        {
+            Assert.True(Year.IsLeapYear(year));
+        }
+    }
+}
