52ad20e0a56b7ca0bf124602cd009aaafba61c2e
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-10 10:11:55 +0200

    false cases

--- Tasks/Leap_year/LeapYear.Tests/YearTests.cs
+++ Tasks/Leap_year/LeapYear.Tests/YearTests.cs
@@ -5,12 +5,15 @@ namespace LeapYearTask.Tests
     [TestFixture]
     public class YearTests
     {
-
+        [TestCase(2015)]
+        [TestCase(1970)]
+        [TestCase(2100)]
+        [TestCase(1800)]
+        [TestCase(1234)]
         public void IsLeapYear_ReturnFalse(int year)
         {
             Assert.False(Year.IsLeapYear(year));
         }
-
 
         public void IsLeapYear_ReturnTrue(int year)
         {
