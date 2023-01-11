0f6da32ae28fff47aeb740bf3a85fdf02b197b7e
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-11 10:04:55 +0200

    lands in the border

--- Tasks/Darts_game/DartsGame.Tests/DartsTests.cs
+++ Tasks/Darts_game/DartsGame.Tests/DartsTests.cs
@@ -10,5 +10,12 @@ namespace DartsGame.Tests
         {
             Assert.AreEqual(0, Darts.GetScore(-9, 9));
         }
-  }
+
+        [Test]
+        public void GetScore_DartLandsJustInTheBorderOfTheTarget_Return_1()
+        {
+            Assert.AreEqual(1, Darts.GetScore(0, 10));
+        }
+
+    }
 }
