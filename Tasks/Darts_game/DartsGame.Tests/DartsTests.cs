968791717ae9b7138d21976d9c9e7f8a80068d2e
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-11 10:04:29 +0200

    lands outside

---
+++ Tasks/Darts_game/DartsGame.Tests/DartsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DartsGame.Tests
+{
+    [TestFixture]
+    public class DartsTests
+    {
+        [Test]
+        public void GetScore_DartLandsOutsideTheTarget_Return_0()
+        {
+            Assert.AreEqual(0, Darts.GetScore(-9, 9));
+        }
+  }
+}
