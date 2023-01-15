cca61283678d923622be747dd955bfbdaac0e646
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-15 12:22:00 +0200

    darts game

---
+++ Tasks/Darts_game/DartsGame/Darts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DartsGame
+{
+    public static class Darts
+    {
+        public static int GetScore(double x, double y)
+        {
+
+        }
+    }
+}
