cb31f778ea625cfae0205932a973268327a84a16
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-15 12:23:21 +0200

    xml comments

--- Tasks/Darts_game/DartsGame/Darts.cs
+++ Tasks/Darts_game/DartsGame/Darts.cs
@@ -5,6 +5,12 @@ namespace DartsGame
 {
     public static class Darts
     {
+        /// <summary>
+        /// Calculates the earned points in a single toss of a Darts game.
+        /// </summary>
+        /// <param name="x">x-coordinate of dart.</param>
+        /// <param name="y">y-coordinate of dart.</param>
+        /// <returns>The earned points.</returns>
         public static int GetScore(double x, double y)
         {
             double r = Math.Sqrt((x * x) + (y * y));
