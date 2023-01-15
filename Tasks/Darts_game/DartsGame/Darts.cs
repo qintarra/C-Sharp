6465547b76842b0f9b1f7022f2b36248855566c9
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-15 12:22:48 +0200

    get score method

--- Tasks/Darts_game/DartsGame/Darts.cs
+++ Tasks/Darts_game/DartsGame/Darts.cs
@@ -7,7 +7,25 @@ namespace DartsGame
     {
         public static int GetScore(double x, double y)
         {
+            double r = Math.Sqrt((x * x) + (y * y));
+            if (r <= 1.0)
+            {
+                return 10;
+            }
 
+            if (r <= 5.0)
+            {
+                return 5;
+            }
+
+            if (r <= 10.0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
