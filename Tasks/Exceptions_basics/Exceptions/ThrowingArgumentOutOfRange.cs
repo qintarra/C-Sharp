db8ab0f1b9c03085086b001e109fb4e5403acd3e
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-28 11:05:16 +0200

    task 1

---
+++ Tasks/Exceptions_basics/Exceptions/ThrowingArgumentOutOfRange.cs
@@ -0,0 +1,17 @@
+﻿namespace Exceptions
+{
+    public static class ThrowingArgumentOutOfRange
+    {
+        public static bool CheckParameterAndThrowException1(int i)
+        {
+            if (i <= -5 || i >= 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(i));
+            }
+
+            return true;
+        }
+
+        }
+    }
+}
