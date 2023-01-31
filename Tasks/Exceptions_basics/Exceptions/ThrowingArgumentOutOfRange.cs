b02228c78637ecc529c3cc6ed695c2bb36591096
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-31 11:32:33 +0200

    task 4

--- Tasks/Exceptions_basics/Exceptions/ThrowingArgumentOutOfRange.cs
+++ Tasks/Exceptions_basics/Exceptions/ThrowingArgumentOutOfRange.cs
@@ -40,6 +40,19 @@
             return true;
         }
 
+        public static bool CheckParametersAndThrowException4(long l, float f)
+        {
+            if (l < -9 || (l >= -3 && l < 3) || l >= 9)
+            {
+                throw new ArgumentOutOfRangeException(nameof(l), "l should be in [-9, -3) or [3, 9) intervals.");
+            }
 
+            if (f < -0.3 || f > 0.3)
+            {
+                throw new ArgumentOutOfRangeException(nameof(f), "f should be in the (-0.3, 0.3) interval.");
+            }
+
+            return true;
+        }
     }
 }
