6a79fcc2b6925dfd9712a334b7841c33373c00cc
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-01 11:46:42 +0200

    task 2-1

---
+++ Tasks/Exceptions_basics/Exceptions/ThrowingArgumentNull.cs
@@ -0,0 +1,17 @@
+﻿namespace Exceptions
+{
+    public static class ThrowingArgumentNull
+    {
+        public static bool CheckParameterAndThrowException1(object o)
+        {
+            // 2-1. Add the code to throw the ArgumentNullException if the o argument is null.
+            if (o is null)
+            {
+                throw new ArgumentNullException(nameof(o));
+            }
+
+            return true;
+        }
+
+    }
+}
