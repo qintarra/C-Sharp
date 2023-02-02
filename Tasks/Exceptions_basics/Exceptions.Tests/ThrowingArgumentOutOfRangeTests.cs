df2dc633409e1f8f12c664d6983954498152c245
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-02 12:39:56 +0200

    unit tests

---
+++ Tasks/Exceptions_basics/Exceptions.Tests/ThrowingArgumentOutOfRangeTests.cs
@@ -0,0 +1,37 @@
+﻿global using NUnit.Framework;
+
+namespace Exceptions.Tests
+{
+    [TestFixture]
+    public class ThrowingArgumentOutOfRangeTests
+    {
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(-5)]
+        [TestCase(-6)]
+        [TestCase(-7)]
+        [TestCase(-8)]
+        [TestCase(-9)]
+        [TestCase(-10)]
+        public void CheckParameterAndThrowException1_ThrowsArgumentOutOfRangeException(int i)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                try
+                {
+                    ThrowingArgumentOutOfRange.CheckParameterAndThrowException1(i);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Assert.AreEqual(nameof(i), e.ParamName);
+                    throw;
+                }
+            });
+        }
+
+    }
+}
