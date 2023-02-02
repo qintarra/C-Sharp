46fddfad44612af946bbcf00576b80b61d9ec811
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-03 00:32:52 +0200

    IsNull test

---
+++ Tasks/Exceptions_basics/Exceptions.Tests/ThrowingArgumentNullTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Exceptions.Tests
+{
+    [TestFixture]
+    public class ThrowingArgumentNullTests
+    {
+        [Test]
+        public void CheckParameterAndThrowException1_O_IsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                try
+                {
+                    ThrowingArgumentNull.CheckParameterAndThrowException1(null);
+                }
+                catch (ArgumentNullException e)
+                {
+                    Assert.AreEqual("o", e.ParamName);
+                    throw;
+                }
+            });
+        }
+
+    }
+}
