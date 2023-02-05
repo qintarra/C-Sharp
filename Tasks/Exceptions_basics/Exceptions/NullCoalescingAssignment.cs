d4bd0b1cb2936b88f522abbb91d156eececff028
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-06 00:06:04 +0200

    task 1

---
+++ Tasks/Exceptions_basics/Exceptions/NullCoalescingAssignment.cs
@@ -0,0 +1,13 @@
+﻿namespace Exceptions
+{
+    public static class NullCoalescingAssignment
+    {
+        public static object CheckParameterAndThrowException1(object o)
+        {
+            o ??= new object();
+
+            return o;
+        }
+
+    }
+}
