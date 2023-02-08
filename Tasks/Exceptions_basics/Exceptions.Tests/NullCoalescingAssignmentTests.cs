9bcd789be21d94052288dc0a826f92a84c5714db
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-08 11:30:00 +0200

    is null 1

--- Tasks/Exceptions_basics/Exceptions.Tests/NullCoalescingAssignmentTests.cs
+++ Tasks/Exceptions_basics/Exceptions.Tests/NullCoalescingAssignmentTests.cs
@@ -2,6 +2,15 @@
 {
     public class NullCoalescingAssignmentTests
     {
+        [Test]
+        public void CheckParameterAndThrowException1_O_IsNull_ReturnsObject()
+        {
+            // Act
+            object actualResult = NullCoalescingAssignment.CheckParameterAndThrowException1(null);
+
+            // Assert
+            Assert.IsNotNull(actualResult);
+        }
 
     }
 }
