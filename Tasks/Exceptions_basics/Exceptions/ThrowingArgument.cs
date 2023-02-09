93cd1d5f1acbf58fab7abb61b197ec66b523058c
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-09 09:25:12 +0200

    task 1

--- Tasks/Exceptions_basics/Exceptions/ThrowingArgument.cs
+++ Tasks/Exceptions_basics/Exceptions/ThrowingArgument.cs
@@ -2,6 +2,15 @@
 {
     public static class ThrowingArgument
     {
+        public static int CheckParameterAndThrowException1(int i)
+        {
+            if (i % 2 != 0)
+            {
+                throw new ArgumentException("i should not be odd.", nameof(i));
+            }
+
+            return i;
+        }
 
     }
 }
