abccde1458b1b538bd6594d2958bf7a4aa5b344d
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-18 13:51:25 +0200

    task 1

---
+++ Tasks/Exceptions_basics/Exceptions/HandlingExceptions.cs
@@ -0,0 +1,19 @@
+﻿namespace Exceptions
+{
+    public static class HandlingExceptions
+    {
+        public static bool CatchArgumentOutOfRangeException1(int i, Func<int, bool> foo)
+        {
+            // 5-1. Add the try-catch statement to catch an exception of ArgumentOutOfRangeException type. The method should return false if an exception is thrown.
+            try
+            {
+                return foo(i);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return false;
+            }
+        }
+
+    }
+}
