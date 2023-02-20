bea754a3ca9618880cdb58d66b5ec8ff87627ccc
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-20 14:59:52 +0200

    task 1 TrowsExeption

---
+++ Tasks/Exceptions_basics/Exceptions.Tests/HandlingExceptionsTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Exceptions.Tests
+{
+    [TestFixture]
+    public class HandlingExceptionsTests
+    {
+        [Test]
+        public void CatchArgumentOutOfRangeException1_ExceptionIsThrown_ThrowsException()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                HandlingExceptions.CatchArgumentOutOfRangeException1(1, TestArgumentOutOfRangeException);
+            });
+        }
+
+    }
+}
