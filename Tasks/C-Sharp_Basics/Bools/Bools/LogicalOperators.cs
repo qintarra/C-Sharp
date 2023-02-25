328f64e0424e762de47a91f04eb6aba01011c9b1
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-25 16:22:36 +0200

    logiacl and

---
+++ Tasks/C-Sharp_Basics/Bools/Bools/LogicalOperators.cs
@@ -0,0 +1,24 @@
+﻿namespace Bools
+{
+    public static class LogicalOperators
+    {
+        public static bool LogicalAnd1(bool b1, bool b2)
+        {
+            // Return the result of logical AND for b1 and b2 parameters.
+            return b1 && b2;
+        }
+
+        public static bool LogicalAnd2(bool b1, bool b2, bool b3)
+        {
+            // Return the result of logical AND for b1, b2 and b3 parameters.
+            return b1 && b2 && b3;
+        }
+
+        public static bool LogicalAnd3(bool b1, bool b2, bool b3, bool b4)
+        {
+            // Return the result of logical AND for b1, b2, b3 and b4 parameters.
+            return b1 && b2 && b3 && b4;
+        }
+
+    }
+}
