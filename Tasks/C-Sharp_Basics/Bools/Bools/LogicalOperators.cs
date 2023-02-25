ae87b7a6eebf6847d4ec919f6dd7dbdfeefcd38b
qintarra <96946215+qintarra@users.noreply.github.com>
2023-02-25 16:23:01 +0200

    logiacl x or

--- Tasks/C-Sharp_Basics/Bools/Bools/LogicalOperators.cs
+++ Tasks/C-Sharp_Basics/Bools/Bools/LogicalOperators.cs
@@ -38,5 +38,23 @@
             return b1 || b2 || b3 || b4;
         }
 
+        public static bool LogicalXor1(bool b1, bool b2)
+        {
+            // Return the result of logical exclusive OR for b1 and b2 parameters.
+            return b1 ^ b2;
+        }
+
+        public static bool LogicalXor2(bool b1, bool b2, bool b3)
+        {
+            // Return the result of logical exclusive OR for b1, b2 and b3 parameters.
+            return b1 ^ b2 ^ b3;
+        }
+
+        public static bool LogicalXor3(bool b1, bool b2, bool b3, bool b4)
+        {
+            // Return the result of logical AND for b1, b2, b3 and b4 parameters.
+            return b1 ^ b2 ^ b3 ^ b4;
+        }
+
     }
 }
