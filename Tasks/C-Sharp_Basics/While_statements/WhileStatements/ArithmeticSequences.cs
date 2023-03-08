1f82b3e155440b48fc659b98acf4be7a90412e84
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-09 00:17:33 +0200

    task 2

--- Tasks/C-Sharp_Basics/While_statements/WhileStatements/ArithmeticSequences.cs
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/ArithmeticSequences.cs
@@ -16,5 +16,21 @@
             return sum;
         }
 
+        public static int SumArithmeticSequenceTerms2(int n)
+        {
+            // 1-2. Implement the method that returns sum of an arithmetic sequence terms.
+            const int firstTerm = 17;
+            const int commonDifference = 33;
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += firstTerm + (i * commonDifference);
+                i++;
+            }
+
+            return sum;
+        }
+
     }
 }
