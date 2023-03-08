11f9846bfd7424fb3f4071853e344d10a6e805c4
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-09 00:10:30 +0200

    task 1

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/ArithmeticSequences.cs
@@ -0,0 +1,20 @@
+﻿namespace WhileStatements
+{
+    public static class ArithmeticSequences
+    {
+        public static int SumArithmeticSequenceTerms1(int a, int n)
+        {
+            // 1-1. Implement the method that returns sum of an arithmetic sequence terms.
+            int sum = 0, i = 0;
+
+            while (i < n)
+            {
+                sum += a + i;
+                i++;
+            }
+
+            return sum;
+        }
+
+    }
+}
