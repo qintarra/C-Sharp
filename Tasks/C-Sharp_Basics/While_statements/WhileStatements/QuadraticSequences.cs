8e4d2578876adc7d290faa34f06254e5c27924c6
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-10 00:21:56 +0200

    quadratic sequences created

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/QuadraticSequences.cs
@@ -0,0 +1,15 @@
+﻿namespace WhileStatements
+{
+    public static class QuadraticSequences
+    {
+        public static long SumQuadraticSequenceTerms1(long a, long b, long c, long maxTerm)
+        {
+
+        }
+
+        public static long SumQuadraticSequenceTerms2(long a, long b, long c, long startN, long count)
+        {
+
+        }
+    }
+}
