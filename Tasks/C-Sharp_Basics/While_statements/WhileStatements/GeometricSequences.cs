487c267a78aaa20d580595dacbb05f1c2cdbe2b0
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-10 00:24:20 +0200

    sum of geometric sequence terms

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/GeometricSequences.cs
@@ -0,0 +1,28 @@
+﻿namespace WhileStatements
+{
+    public static class GeometricSequences
+    {
+        public static uint SumGeometricSequenceTerms1(uint a, uint r, uint n)
+        {
+            // 3-1. Implement the method that returns sum of a geometric sequence terms.
+            uint sum = 0, i = 0;
+
+            while (i < n)
+            {
+                uint j = 0, rpow = 1;
+
+                while (j < i)
+                {
+                    rpow *= r;
+                    j++;
+                }
+
+                sum += a * rpow;
+                i++;
+            }
+
+            return sum;
+        }
+
+    }
+}
