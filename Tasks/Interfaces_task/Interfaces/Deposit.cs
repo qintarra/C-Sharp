fd55a34a6d616150e72a842d84e1893362e85a63
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-11 12:01:49 +0200

    Deposit.cs moved

---
+++ Tasks/Interfaces_task/Interfaces/Deposit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public abstract class Deposit : IComparable<Deposit>, IProlongable
+    {
+        public decimal Amount { get; }
+        public int Period { get; }
+        public Deposit(decimal amount, int period)
+        {
+            Amount = amount;
+            Period = period;
+        }
+        public abstract decimal Income();
+        public int CompareTo(Deposit deposit)
+        {
+            return (Amount + Income()).CompareTo(deposit.Amount + deposit.Income());
+        }
+        public virtual bool CanToProlong()
+        {
+           return false;
+        }
+    }
+}
