a048cb3e4f0e39af7651f4ee424288c876c1b9ef
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-13 00:16:33 +0200

    geometric test2

--- Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/GeometricSequencesTests.cs
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/GeometricSequencesTests.cs
@@ -33,5 +33,25 @@
             return GeometricSequences.SumGeometricSequenceTerms1(a, r, n);
         }
 
+        [TestCase(1u, ExpectedResult = 13u)]
+        [TestCase(2u, ExpectedResult = 52u)]
+        [TestCase(3u, ExpectedResult = 169u)]
+        [TestCase(4u, ExpectedResult = 520u)]
+        [TestCase(5u, ExpectedResult = 1573u)]
+        [TestCase(6u, ExpectedResult = 4732u)]
+        [TestCase(7u, ExpectedResult = 14209u)]
+        [TestCase(8u, ExpectedResult = 42640u)]
+        [TestCase(9u, ExpectedResult = 127933u)]
+        [TestCase(10u, ExpectedResult = 383812u)]
+        [TestCase(11u, ExpectedResult = 1151449u)]
+        [TestCase(12u, ExpectedResult = 3454360u)]
+        [TestCase(13u, ExpectedResult = 10363093u)]
+        [TestCase(14u, ExpectedResult = 31089292u)]
+        [TestCase(15u, ExpectedResult = 93267889u)]
+        public uint SumGeometricSequenceTerms2_ReturnsSum(uint n)
+        {
+            return GeometricSequences.SumGeometricSequenceTerms2(n);
+        }
+
     }
 }
