7d338df33245f9ff474773bee063d626af5d7f76
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-12 14:07:56 +0200

    quadratic tests

--- Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/QuadraticSequencesTests.cs
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/QuadraticSequencesTests.cs
@@ -22,5 +22,35 @@
         {
             return QuadraticSequences.SumQuadraticSequenceTerms1(a, b, c, maxTerm);
         }
+
+        [TestCase(3, 5, 7, 1, 1, ExpectedResult = 15)]
+        [TestCase(3, 5, 7, 1, 2, ExpectedResult = 44)]
+        [TestCase(3, 5, 7, 1, 3, ExpectedResult = 93)]
+        [TestCase(3, 5, 7, 1, 4, ExpectedResult = 168)]
+        [TestCase(3, 5, 7, 1, 5, ExpectedResult = 275)]
+        [TestCase(3, 5, 7, 1, 6, ExpectedResult = 420)]
+        [TestCase(3, 5, 7, 1, 7, ExpectedResult = 609)]
+        [TestCase(3, 5, 7, 1, 8, ExpectedResult = 848)]
+        [TestCase(3, 5, 7, 1, 9, ExpectedResult = 1143)]
+        [TestCase(3, 5, 7, 1, 10, ExpectedResult = 1500)]
+        [TestCase(3, 5, 7, 2, 1, ExpectedResult = 29)]
+        [TestCase(3, 5, 7, 2, 2, ExpectedResult = 78)]
+        [TestCase(3, 5, 7, 2, 3, ExpectedResult = 153)]
+        [TestCase(3, 5, 7, 2, 4, ExpectedResult = 260)]
+        [TestCase(3, 5, 7, 2, 5, ExpectedResult = 405)]
+        [TestCase(3, 5, 7, 2, 6, ExpectedResult = 594)]
+        [TestCase(3, 5, 7, 2, 7, ExpectedResult = 833)]
+        [TestCase(3, 5, 7, 2, 8, ExpectedResult = 1128)]
+        [TestCase(3, 5, 7, 2, 9, ExpectedResult = 1485)]
+        [TestCase(3, 5, 7, 8, 1, ExpectedResult = 239)]
+        [TestCase(3, 5, 7, 8, 2, ExpectedResult = 534)]
+        [TestCase(3, 5, 7, 8, 3, ExpectedResult = 891)]
+        [TestCase(3, 5, 7, 9, 1, ExpectedResult = 295)]
+        [TestCase(3, 5, 7, 9, 2, ExpectedResult = 652)]
+        [TestCase(3, 5, 7, 10, 1, ExpectedResult = 357)]
+        public long SumQuadraticSequenceTerms2_ReturnsSum(long a, long b, long c, long startN, long count)
+        {
+            return QuadraticSequences.SumQuadraticSequenceTerms2(a, b, c, startN, count);
+        }
     }
 }
