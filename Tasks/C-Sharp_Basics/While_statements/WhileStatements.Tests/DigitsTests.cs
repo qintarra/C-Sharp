5ff838e33263492a59922fd09114dd557ba9d8be
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-14 00:12:33 +0200

    digits product tests

--- Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/DigitsTests.cs
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/DigitsTests.cs
@@ -41,5 +41,37 @@
             return Digits.GetDigitsSum(l);
         }
 
+        [TestCase(0ul, ExpectedResult = 0ul)]
+        [TestCase(1ul, ExpectedResult = 1ul)]
+        [TestCase(2ul, ExpectedResult = 2ul)]
+        [TestCase(3ul, ExpectedResult = 3ul)]
+        [TestCase(4ul, ExpectedResult = 4ul)]
+        [TestCase(5ul, ExpectedResult = 5ul)]
+        [TestCase(6ul, ExpectedResult = 6ul)]
+        [TestCase(7ul, ExpectedResult = 7ul)]
+        [TestCase(8ul, ExpectedResult = 8ul)]
+        [TestCase(9ul, ExpectedResult = 9ul)]
+        [TestCase(10ul, ExpectedResult = 0ul)]
+        [TestCase(11ul, ExpectedResult = 1ul)]
+        [TestCase(12ul, ExpectedResult = 2ul)]
+        [TestCase(13ul, ExpectedResult = 3ul)]
+        [TestCase(20ul, ExpectedResult = 0ul)]
+        [TestCase(21ul, ExpectedResult = 2ul)]
+        [TestCase(22ul, ExpectedResult = 4ul)]
+        [TestCase(23ul, ExpectedResult = 6ul)]
+        [TestCase(30ul, ExpectedResult = 0ul)]
+        [TestCase(31ul, ExpectedResult = 3ul)]
+        [TestCase(32ul, ExpectedResult = 6ul)]
+        [TestCase(33ul, ExpectedResult = 9ul)]
+        [TestCase(123ul, ExpectedResult = 6ul)]
+        [TestCase(231ul, ExpectedResult = 6ul)]
+        [TestCase(1234ul, ExpectedResult = 24ul)]
+        [TestCase(2314ul, ExpectedResult = 24ul)]
+        [TestCase(12345ul, ExpectedResult = 120ul)]
+        [TestCase(23145ul, ExpectedResult = 120ul)]
+        public ulong GetProduct_ReturnsProduct(ulong l)
+        {
+            return Digits.GetDigitsProduct(l);
+        }
     }
 }
