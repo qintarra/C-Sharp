932f73f1a5d05195b135fed5756576d096be3590
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-14 00:10:25 +0200

    is prime number

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/PrimeNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace WhileStatements
+{
+    public static class PrimeNumbers
+    {
+        public static bool IsPrimeNumber(uint n)
+        {
+            // 5-1. Implement the method that returns true if n is a prime number; otherwise returns false.
+            if (n < 2)
+            {
+                return false;
+            }
+
+            uint i = 2;
+
+            while (i * i <= n)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+
+                i++;
+            }
+
+            return true;
+        }
+
+    }
+}
