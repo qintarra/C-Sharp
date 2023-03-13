66e41082b364b52399588c3757ca31211b65d4ef
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-14 00:09:34 +0200

    product of digits

--- Tasks/C-Sharp_Basics/While_statements/WhileStatements/Digits.cs
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/Digits.cs
@@ -7,5 +7,23 @@
             // 4-1. Implement the method that returns sum of digits of a number.
             return (n / 100) + ((n / 10) % 10) + (n % 10);
         }
+
+        public static ulong GetDigitsProduct(ulong n)
+        {
+            // 4-2. Implement the method that returns product of digits of a number.
+            if (n == ulong.MinValue)
+            {
+                return 0;
+            }
+
+            ulong product = 1;
+            while (n > 0)
+            {
+                product *= n % 10;
+                n /= 10;
+            }
+
+            return product;
+        }
     }
 }
