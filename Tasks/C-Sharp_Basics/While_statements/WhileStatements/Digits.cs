8b2a19d3226d7c8d9ce7b55d0eb1246f49f09de4
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-14 00:09:12 +0200

    sum of digits

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements/Digits.cs
@@ -0,0 +1,11 @@
+﻿namespace WhileStatements
+{
+    public static class Digits
+    {
+        public static ulong GetDigitsSum(ulong n)
+        {
+            // 4-1. Implement the method that returns sum of digits of a number.
+            return (n / 100) + ((n / 10) % 10) + (n % 10);
+        }
+    }
+}
