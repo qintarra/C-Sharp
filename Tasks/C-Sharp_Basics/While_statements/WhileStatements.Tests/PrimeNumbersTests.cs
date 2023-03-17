5beacecb1321d3762ccff29b0ab63bdb48297dff
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-17 12:28:46 +0200

    IsPrime test

---
+++ Tasks/C-Sharp_Basics/While_statements/WhileStatements.Tests/PrimeNumbersTests.cs
@@ -0,0 +1,43 @@
+﻿namespace WhileStatements.Tests
+{
+    [TestFixture]
+    public class PrimeNumbersTests
+    {
+        [TestCase(0u, ExpectedResult = false)]
+        [TestCase(1u, ExpectedResult = false)]
+        [TestCase(2u, ExpectedResult = true)]
+        [TestCase(3u, ExpectedResult = true)]
+        [TestCase(4u, ExpectedResult = false)]
+        [TestCase(5u, ExpectedResult = true)]
+        [TestCase(6u, ExpectedResult = false)]
+        [TestCase(7u, ExpectedResult = true)]
+        [TestCase(8u, ExpectedResult = false)]
+        [TestCase(9u, ExpectedResult = false)]
+        [TestCase(10u, ExpectedResult = false)]
+        [TestCase(11u, ExpectedResult = true)]
+        [TestCase(12u, ExpectedResult = false)]
+        [TestCase(13u, ExpectedResult = true)]
+        [TestCase(14u, ExpectedResult = false)]
+        [TestCase(15u, ExpectedResult = false)]
+        [TestCase(16u, ExpectedResult = false)]
+        [TestCase(17u, ExpectedResult = true)]
+        [TestCase(18u, ExpectedResult = false)]
+        [TestCase(19u, ExpectedResult = true)]
+        [TestCase(20u, ExpectedResult = false)]
+        [TestCase(21u, ExpectedResult = false)]
+        [TestCase(22u, ExpectedResult = false)]
+        [TestCase(23u, ExpectedResult = true)]
+        [TestCase(29u, ExpectedResult = true)]
+        [TestCase(31u, ExpectedResult = true)]
+        [TestCase(37u, ExpectedResult = true)]
+        [TestCase(41u, ExpectedResult = true)]
+        [TestCase(43u, ExpectedResult = true)]
+        [TestCase(47u, ExpectedResult = true)]
+        [TestCase(53u, ExpectedResult = true)]
+        public bool IsPrimeNumber(uint n)
+        {
+            return PrimeNumbers.IsPrimeNumber(n);
+        }
+
+    }
+}
