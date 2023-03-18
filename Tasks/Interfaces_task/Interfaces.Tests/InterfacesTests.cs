50476eefd581be43762a8a0046c56505fb039687
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-18 10:05:29 +0200

    interface tests created

---
+++ Tasks/Interfaces_task/Interfaces.Tests/InterfacesTests.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Interfaces.Tests
+{
+
+}
