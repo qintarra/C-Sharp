2eb6c0c1e9d78a6c7ba493963cf323654e9e0ba2
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-18 09:59:04 +0200

    long deposit

---
+++ Tasks/Interfaces_task/Interfaces/LongDeposit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public class LongDeposit : Deposit
+    {
+        public LongDeposit(decimal amount, int period) : base(amount, period)
+        {
+
+        }
+        public override decimal Income()
+        {
+            decimal a = Amount;
+
+            for (int i = 7; i <= Period; i++)
+            {
+                a += a * 0.15m;
+            }
+            return a - Amount;
+        }
+        public override bool CanToProlong()
+        {
+            if (Period <= 36)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
