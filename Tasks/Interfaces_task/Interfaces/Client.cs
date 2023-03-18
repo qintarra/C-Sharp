ec52ec333996fe22f71dd320228ea4b1a069c6fc
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-18 10:01:40 +0200

    client

---
+++ Tasks/Interfaces_task/Interfaces/Client.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public class Client
+    {
+        private readonly Deposit[] deposits;
+        public Client()
+        {
+            deposits = new Deposit[10];
+        }
+        public bool AddDeposit(Deposit deposit)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (deposits[i] == null)
+                {
+                    deposits[i] = deposit;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public decimal TotalIncome()
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (deposits[i] != null)
+                {
+                    sum += deposits[i].Income();
+                }
+            }
+            return sum;
+        }
+        public decimal MaxIncome()
+        {
+            decimal max = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (deposits[i] != null && max < deposits[i].Income())
+                    max = deposits[i].Income();
+            }
+            return max;
+        }
+        public decimal GetIncomeByNumber(int i)
+        {
+            if (deposits[i - 1] != null)
+            {
+                return deposits[i - 1].Income();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        }
+    }
+}
