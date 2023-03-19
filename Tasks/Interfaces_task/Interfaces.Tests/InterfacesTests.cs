7cd38481d351e5c866f010b248f8be366710c95f
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-19 10:27:47 +0200

    Income works

--- Tasks/Interfaces_task/Interfaces.Tests/InterfacesTests.cs
+++ Tasks/Interfaces_task/Interfaces.Tests/InterfacesTests.cs
@@ -27,5 +27,35 @@ namespace Interfaces.Tests
             return type;
         }
 
+        [TestCase("BaseDeposit", 1000, 0, 0)]
+        [TestCase("BaseDeposit", 1000, 1, 50)]
+        [TestCase("BaseDeposit", 1000, 2, 102.5)]
+        [TestCase("BaseDeposit", 1000, 3, 157.625)]
+        [TestCase("SpecialDeposit", 1000, 0, 0)]
+        [TestCase("SpecialDeposit", 1000, 1, 10)]
+        [TestCase("SpecialDeposit", 1000, 2, 30.2)]
+        [TestCase("SpecialDeposit", 1000, 3, 61.106)]
+        [TestCase("LongDeposit", 1000, 0, 0)]
+        [TestCase("LongDeposit", 1000, 1, 0)]
+        [TestCase("LongDeposit", 1000, 6, 0)]
+        [TestCase("LongDeposit", 1000, 7, 150)]
+        [TestCase("LongDeposit", 1000, 8, 322.5)]
+        public void Class_Income_WorksCorrectly(string className, decimal summ, int period, decimal expectedAmountAfterPeriod)
+        {
+            var type = Type.GetType($"{AssemblyName}.{className}, {AssemblyName}");
+            decimal precision = 0.01m;
+            AssertFailIfNull(type, $"Class '{className}'");
+
+            var income = type.GetMethod("Income");
+            AssertFailIfNull(income, "Method 'Income'");
+
+            var deposit = Activator.CreateInstance(type, summ, period);
+
+            if (Math.Abs(expectedAmountAfterPeriod - (decimal)income.Invoke(deposit, null)) > precision)
+            {
+                Assert.Fail($"Method 'Income' in {className} class works incorrectly.");
+            }
+        }
+
     }
 }
