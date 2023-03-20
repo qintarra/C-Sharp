0c80f665e728512dfb790fad58f8c084f6d961a9
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-20 10:27:58 +0200

    task 4

---
+++ Tasks/C-Sharp_Basics/While_practice/WhilePractice/Task4.cs
@@ -0,0 +1,28 @@
+﻿namespace WhilePractice
+{
+    public static class Task4
+    {
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/(3 * 3) + 1/(5 * 5) + 1/(7 * 7) + ... + 1/((2 * n + 1) * (2 * n + 1)), where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double SumSequenceElements(int n)
+        {
+            double sum = 0;
+            int i = 1;
+            double power = 3;
+
+            while (i <= n)
+            {
+                double term = 1.0 / (power * power);
+                sum += term;
+                i++;
+                power += 2;
+            }
+
+            return sum;
+        }
+    }
+}
