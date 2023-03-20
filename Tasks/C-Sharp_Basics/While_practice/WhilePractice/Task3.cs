21f92a1aa7572b33fb0820555ca986d37e35ebcf
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-20 10:27:48 +0200

    task 3

---
+++ Tasks/C-Sharp_Basics/While_practice/WhilePractice/Task3.cs
@@ -0,0 +1,26 @@
+﻿namespace WhilePractice
+{
+    public static class Task3
+    {
+        /// <summary>
+        /// Calculate the following sum
+        /// 1/1^5 + 1/2^5 + 1/3^5 + ... + 1/n^5, where n > 0.
+        /// </summary>
+        /// <param name="n">Number of elements.</param>
+        /// <returns>Sum of elements.</returns>
+        public static double SumSequenceElements(int n)
+        {
+            double sum = 0;
+            int i = 1;
+
+            while (i <= n)
+            {
+                double term = 1.0 / Math.Pow(i, 5);
+                sum += term;
+                i++;
+            }
+
+            return sum;
+        }
+    }
+}
