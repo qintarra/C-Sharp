27249925df6489610d6068a6bf0bc33597571ead
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-22 00:13:22 +0200

    task 1

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/ArithmeticSequences.cs
@@ -0,0 +1,19 @@
+﻿namespace ForStatements
+{
+    public static class ArithmeticSequences
+    {
+        public static int SumArithmeticSequenceTerms1(int a, int n)
+        {
+            // Task 1. Implement the method that returns sum of an arithmetic sequence terms.
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += a + i;
+            }
+
+            return sum;
+        }
+
+    }
+}
