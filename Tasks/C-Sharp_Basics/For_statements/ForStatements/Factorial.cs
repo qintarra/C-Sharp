ead4faf4ff2ae094aa37a5527059880025d0f0de
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-22 00:14:33 +0200

    task 1

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/Factorial.cs
@@ -0,0 +1,23 @@
+﻿namespace ForStatements
+{
+    public static class Factorial
+    {
+        public static int GetFactorial(int n)
+        {
+            // Task 4. Implement the method that returns the factorial of the number n! using the for statement.
+            int factorial = 1;
+
+            if (n == 0)
+            {
+                return factorial;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                factorial *= i;
+            }
+
+            return n * factorial;
+        }
+    }
+}
