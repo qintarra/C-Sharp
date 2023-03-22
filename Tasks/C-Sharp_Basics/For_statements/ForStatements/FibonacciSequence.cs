c7d3bdf00c75cd57ff0617a58a0f43557afc4016
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-23 00:06:08 +0200

    task 8

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/FibonacciSequence.cs
@@ -0,0 +1,22 @@
+﻿namespace ForStatements
+{
+    public static class FibonacciSequence
+    {
+        public static int GetFibonacciNumber(int n)
+        {
+            // Task 9. Implement the method that returns a Fibonacci sequence number.
+            int current = 0;
+            int next = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int temp = current;
+                current = next;
+                next = temp + next;
+            }
+
+            return current;
+        }
+
+    }
+}
