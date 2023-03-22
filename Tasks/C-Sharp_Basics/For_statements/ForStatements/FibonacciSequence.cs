cc71c4a75b1c4929ff74ab752c851c316e238725
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-23 00:06:20 +0200

    task 9

--- Tasks/C-Sharp_Basics/For_statements/ForStatements/FibonacciSequence.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/FibonacciSequence.cs
@@ -18,5 +18,24 @@
             return current;
         }
 
+        public static ulong GetProductOfFibonacciNumberDigits(ulong n)
+        {
+            // Task 10. Implement the method that returns the product of digits of the Fibonacci sequence number.
+            int fibonacci = GetFibonacciNumber((int)n);
+            int product = 1;
+
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            for (int i = fibonacci; i > 0; i /= 10)
+            {
+                int digit = i % 10;
+                product *= digit;
+            }
+
+            return (ulong)product;
+        }
     }
 }
