56c8ca76f17608962ed1f51388e74bf888925c35
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-23 00:09:05 +0200

    sum task 3 tests

--- Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/ArithmeticSequencesTests.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/ArithmeticSequencesTests.cs
@@ -57,5 +57,42 @@ namespace ForStatements.Tests
             return ArithmeticSequences.SumArithmeticSequenceTerms2(n);
         }
 
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 7)]
+        [TestCase(1, 3, ExpectedResult = 18)]
+        [TestCase(1, 4, ExpectedResult = 34)]
+        [TestCase(1, 5, ExpectedResult = 55)]
+        [TestCase(2, 0, ExpectedResult = 0)]
+        [TestCase(2, 1, ExpectedResult = 2)]
+        [TestCase(2, 2, ExpectedResult = 9)]
+        [TestCase(2, 3, ExpectedResult = 21)]
+        [TestCase(2, 4, ExpectedResult = 38)]
+        [TestCase(2, 5, ExpectedResult = 60)]
+        [TestCase(3, 0, ExpectedResult = 0)]
+        [TestCase(3, 1, ExpectedResult = 3)]
+        [TestCase(3, 2, ExpectedResult = 11)]
+        [TestCase(3, 3, ExpectedResult = 24)]
+        [TestCase(3, 4, ExpectedResult = 42)]
+        [TestCase(3, 5, ExpectedResult = 65)]
+        [TestCase(4, 0, ExpectedResult = 0)]
+        [TestCase(4, 1, ExpectedResult = 4)]
+        [TestCase(4, 2, ExpectedResult = 13)]
+        [TestCase(4, 3, ExpectedResult = 27)]
+        [TestCase(4, 4, ExpectedResult = 46)]
+        [TestCase(4, 5, ExpectedResult = 70)]
+        [TestCase(5, 0, ExpectedResult = 0)]
+        [TestCase(5, 1, ExpectedResult = 5)]
+        [TestCase(5, 2, ExpectedResult = 15)]
+        [TestCase(5, 3, ExpectedResult = 30)]
+        [TestCase(5, 4, ExpectedResult = 50)]
+        [TestCase(5, 5, ExpectedResult = 75)]
+        public int SumArithmeticSequenceTerms3_ReturnsSum(int a, int n)
+        {
+            var result = ArithmeticSequences.SumArithmeticSequenceTerms3(a, n);
+            Console.Write($"[TestCase({a}, {n}, ExpectedResult = {result})]");
+            return result;
+        }
     }
 }
