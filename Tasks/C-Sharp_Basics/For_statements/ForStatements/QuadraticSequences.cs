53e7248c37fc28710796d36bbd920620cbc43315
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-23 00:04:29 +0200

    task 6

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/QuadraticSequences.cs
@@ -0,0 +1,32 @@
+﻿namespace ForStatements
+{
+    public static class QuadraticSequences
+    {
+        public static uint CountQuadraticSequenceTerms(long a, long b, long c, long maxTerm)
+        {
+            // Task 6. Implement the method that returns the number of the quadratic sequence terms that are less than or equals to the maxTerm.
+            uint count = 1;
+
+            for (long i = 1; i < maxTerm; i++)
+            {
+                long term = (a * i * i) + (b * i) + c;
+
+                if (term < maxTerm)
+                {
+                    count++;
+                }
+                else if (i > 0)
+                {
+                    return (uint)i;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
