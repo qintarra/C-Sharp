3066279c4073ced3ddd22f4f07bdb257359233a9
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-24 00:36:56 +0200

    product of number digits tests

--- Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/FibonacciSequenceTests.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/FibonacciSequenceTests.cs
@@ -22,5 +22,25 @@
         {
             return FibonacciSequence.GetFibonacciNumber(n);
         }
+
+        [TestCase(0ul, ExpectedResult = 0ul)]
+        [TestCase(1ul, ExpectedResult = 1ul)]
+        [TestCase(2ul, ExpectedResult = 1ul)]
+        [TestCase(3ul, ExpectedResult = 2ul)]
+        [TestCase(4ul, ExpectedResult = 3ul)]
+        [TestCase(5ul, ExpectedResult = 5ul)]
+        [TestCase(6ul, ExpectedResult = 8ul)]
+        [TestCase(7ul, ExpectedResult = 3ul)]
+        [TestCase(8ul, ExpectedResult = 2ul)]
+        [TestCase(9ul, ExpectedResult = 12ul)]
+        [TestCase(10ul, ExpectedResult = 25ul)]
+        [TestCase(11ul, ExpectedResult = 72ul)]
+        [TestCase(12ul, ExpectedResult = 16ul)]
+        [TestCase(13ul, ExpectedResult = 18ul)]
+        [TestCase(14ul, ExpectedResult = 147ul)]
+        public ulong GetProductOfFibonacciNumberDigits(ulong n)
+        {
+            return FibonacciSequence.GetProductOfFibonacciNumberDigits(n);
+        }
     }
 }
