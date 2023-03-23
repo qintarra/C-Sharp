0d0c720946067d11f1de8b249925c8e434a15a52
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-24 00:29:10 +0200

    SumFactorial tests

--- Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/FactorialTests.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/FactorialTests.cs
@@ -15,5 +15,23 @@
         {
             return Factorial.GetFactorial(n);
         }
+
+        [TestCase(0, ExpectedResult = 1)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 6)]
+        [TestCase(4, ExpectedResult = 6)]
+        [TestCase(5, ExpectedResult = 3)]
+        [TestCase(6, ExpectedResult = 9)]
+        [TestCase(7, ExpectedResult = 9)]
+        [TestCase(8, ExpectedResult = 9)]
+        [TestCase(9, ExpectedResult = 27)]
+        [TestCase(10, ExpectedResult = 27)]
+        [TestCase(11, ExpectedResult = 36)]
+        [TestCase(12, ExpectedResult = 27)]
+        public int SumFactorialDigits(int n)
+        {
+            return Factorial.SumFactorialDigits(n);
+        }
     }
 }
