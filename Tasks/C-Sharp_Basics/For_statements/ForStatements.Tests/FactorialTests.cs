bdeaf07c5e8a974ec627ad2d16d6345570e5e416
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-24 00:27:36 +0200

    GetFactorial tests

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/FactorialTests.cs
@@ -0,0 +1,19 @@
+﻿namespace ForStatements.Tests
+{
+    [TestFixture]
+    public class FactorialTests
+    {
+        [TestCase(0, ExpectedResult = 1)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(2, ExpectedResult = 2)]
+        [TestCase(3, ExpectedResult = 6)]
+        [TestCase(4, ExpectedResult = 24)]
+        [TestCase(5, ExpectedResult = 120)]
+        [TestCase(6, ExpectedResult = 720)]
+        [TestCase(7, ExpectedResult = 5040)]
+        public int GetFactorial(int n)
+        {
+            return Factorial.GetFactorial(n);
+        }
+    }
+}
