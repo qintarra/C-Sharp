737d7a9c2c631bf2faff04f703167b2306608fc2
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-25 09:19:43 +0200

    sum

--- Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/GeometricSequencesTests.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements.Tests/GeometricSequencesTests.cs
@@ -33,5 +33,25 @@
             return GeometricSequences.GetGeometricSequenceTermsProduct(a, r, n);
         }
 
+        [TestCase(1u, ExpectedResult = 5ul)]
+        [TestCase(2u, ExpectedResult = 20ul)]
+        [TestCase(3u, ExpectedResult = 65ul)]
+        [TestCase(4u, ExpectedResult = 200ul)]
+        [TestCase(5u, ExpectedResult = 605ul)]
+        [TestCase(6u, ExpectedResult = 1820ul)]
+        [TestCase(7u, ExpectedResult = 5465ul)]
+        [TestCase(8u, ExpectedResult = 16400ul)]
+        [TestCase(9u, ExpectedResult = 49205ul)]
+        [TestCase(10u, ExpectedResult = 147620ul)]
+        [TestCase(11u, ExpectedResult = 442865ul)]
+        [TestCase(12u, ExpectedResult = 1328600ul)]
+        [TestCase(13u, ExpectedResult = 3985805ul)]
+        [TestCase(14u, ExpectedResult = 11957420ul)]
+        [TestCase(15u, ExpectedResult = 35872265ul)]
+        public ulong SumGeometricSequenceTerms_ReturnsSum(uint n)
+        {
+            return GeometricSequences.SumGeometricSequenceTerms(n);
+        }
+
     }
 }
