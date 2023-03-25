5dca3697b4d6f8784e3c9103ffd323abd98aa214
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-25 09:16:17 +0200

    task 11

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/GeometricSequences.cs
@@ -0,0 +1,26 @@
+﻿namespace ForStatements
+{
+    public static class GeometricSequences
+    {
+        public static ulong GetGeometricSequenceTermsProduct(uint a, uint r, uint n)
+        {
+            // Task 11. Implement the method that returns the product of geometric sequence terms.
+            ulong product = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                uint rpow = 1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    rpow *= r;
+                }
+
+                product *= a * rpow;
+            }
+
+            return product;
+        }
+
+    }
+}
