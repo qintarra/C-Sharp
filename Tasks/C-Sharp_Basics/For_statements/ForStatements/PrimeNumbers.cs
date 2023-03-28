247d55ae80de4d16a78048defec1d55a362b1f89
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-28 11:06:47 +0300

    is prime number

---
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/PrimeNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace ForStatements
+{
+    public static class PrimeNumbers
+    {
+        public static bool IsPrimeNumber(uint n)
+        {
+            // Task 15. Implement the method that returns true if n is a prime number; otherwise returns false.
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (uint i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        }
+    }
+}
