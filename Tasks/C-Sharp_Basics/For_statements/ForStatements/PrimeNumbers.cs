2e7e6004b4671173eb723334705ccd33bd701096
qintarra <96946215+qintarra@users.noreply.github.com>
2023-03-28 11:07:12 +0300

    sum of digits

--- Tasks/C-Sharp_Basics/For_statements/ForStatements/PrimeNumbers.cs
+++ Tasks/C-Sharp_Basics/For_statements/ForStatements/PrimeNumbers.cs
@@ -21,6 +21,26 @@
             return true;
         }
 
+        public static ulong SumDigitsOfPrimeNumbers(int start, int end)
+        {
+            // Task 16. Implement the method that returns the sum of digits of all prime numbers in the [start, end] interval.
+            ulong sum = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                if (!IsPrimeNumber((uint)i))
+                {
+                    continue;
+                }
+
+                for (int num = i; num > 0;)
+                {
+                    sum += (ulong)(num % 10);
+                    num /= 10;
+                }
+            }
+
+            return sum;
         }
     }
 }
