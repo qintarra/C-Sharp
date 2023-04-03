6942c7319cee0c767fc15b6bc33f3403de11ce03
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-03 10:20:03 +0300

    tests created

---
+++ Tasks/Collections_task/CustomArray.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CustomArray.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+ 
+    }
+}
