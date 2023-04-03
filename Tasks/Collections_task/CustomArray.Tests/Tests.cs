a679ef3642188430bf4afc1185656c64c0cbbb8d
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-03 10:20:53 +0300

    array get

--- Tasks/Collections_task/CustomArray.Tests/Tests.cs
+++ Tasks/Collections_task/CustomArray.Tests/Tests.cs
@@ -8,6 +8,19 @@ namespace CustomArray.Tests
     [TestFixture]
     public class Tests
     {
- 
+        
+        [Test()]
+        public void Array_Get_ShouldReturnSameReference()
+        {
+            //arrange
+            var array = new int[] { 1, 2, 3, 4 };
+            int first = -5;
+            //act
+            var custom = new CustomArray<int>(first, array);
+            //assert
+            Assert.AreSame(array, custom.Array, message: "Array property works incorrectly");
+        }
+
+  
     }
 }
