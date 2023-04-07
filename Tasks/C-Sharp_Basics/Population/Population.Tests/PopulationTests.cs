811c0bdc473dda877f3e467b2979a38f8fa44445
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-07 09:43:49 +0300

    count of years

--- Tasks/C-Sharp_Basics/Population/Population.Tests/PopulationTests.cs
+++ Tasks/C-Sharp_Basics/Population/Population.Tests/PopulationTests.cs
@@ -3,5 +3,15 @@ using NUnit.Framework;
 
 namespace PopulationTask.Tests
 {
+    [TestFixture]
+    [Timeout(2_000)]
+    public class PopulationTests
+    {
+        [TestCase(1500, 5, 100, 5000, ExpectedResult = 15)]
+        [TestCase(1500000, 2.5, 10000, 2000000, ExpectedResult = 10)]
+        [TestCase(1500000, 0.25, 1000, 2000000, ExpectedResult = 94)]
+        public int GetYears_Return_Count_Of_Years(int initialPopulation, double percent, int visitors, int currentPopulation)
+            => Population.GetYears(initialPopulation, percent, visitors, currentPopulation);
 
+    }
 }
