0b4e871658694158f0d54501a5016199fcd39811
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-07 09:38:22 +0300

    GetYears method implemented

--- Tasks/C-Sharp_Basics/Population/Population/Population.cs
+++ Tasks/C-Sharp_Basics/Population/Population/Population.cs
@@ -6,6 +6,40 @@ namespace PopulationTask
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            if (initialPopulation <= 0)
+            {
+                throw new ArgumentException("Initial population must be greater than 0.");
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("Number of visitors cannot be less than 0.");
+            }
+
+            if (currentPopulation <= 0)
+            {
+                throw new ArgumentException("Current population must be greater than 0.");
+            }
+
+            if (currentPopulation < initialPopulation)
+            {
+                throw new ArgumentException("Current population must be greater than initial population.");
+            }
+
+            if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent), "Percentage must be between 0 and 100.");
+            }
+
+            int years = 0;
+            double population = initialPopulation;
+
+            for (; population < currentPopulation; years++)
+            {
+                population += (population * percent / 100) + visitors;
+            }
+
+            return years;
         }
     }
 }
