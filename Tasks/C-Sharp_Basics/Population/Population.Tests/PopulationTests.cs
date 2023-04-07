0e0c3fb97b4acb9547f6c5816f60780cc5a24e60
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-07 09:44:27 +0300

    init pop zero or less exeption

--- Tasks/C-Sharp_Basics/Population/Population.Tests/PopulationTests.cs
+++ Tasks/C-Sharp_Basics/Population/Population.Tests/PopulationTests.cs
@@ -13,5 +13,15 @@ namespace PopulationTask.Tests
         public int GetYears_Return_Count_Of_Years(int initialPopulation, double percent, int visitors, int currentPopulation)
             => Population.GetYears(initialPopulation, percent, visitors, currentPopulation);
 
+        [TestCase(0, 0.25, 1000, 2000000)]
+        [TestCase(-100, 0.25, 1000, 2000000)]
+        public void GetYears_InitialPopulation_LessOrEqualsZero_ThrowArgumentException(
+            int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            Assert.Throws<ArgumentException>(
+                () => Population.GetYears(initialPopulation, percent, visitors, currentPopulation),
+                message: "Initial population cannot be less or equals zero.");
+        }
+
     }
 }
