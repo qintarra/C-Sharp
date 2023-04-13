51ba52ef864463c157c82614bd0cb6b2146f8430
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-13 11:03:46 +0300

    empty string count

--- Tasks/C-Sharp_Basics/Array.Counting_elements/CountingArrayElements.Tests/WhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Counting_elements/CountingArrayElements.Tests/WhileMethodsTests.cs
@@ -6,6 +6,13 @@ namespace CountingArrayElements.Tests
     [TestFixture]
     public sealed class WhileMethodsTests
     {
+        private static readonly object[][] GetEmptyStringCountData =
+        {
+            new object[] { new[] { string.Empty, "abc", string.Empty, "def", string.Empty, "ghi", string.Empty }, 4 },
+            new object[] { new[] { string.Empty, string.Empty, string.Empty }, 3 },
+            new object[] { new[] { "ab", "cd", "ef", "gh", "ij", "kl", "mn" }, 0 },
+            new object[] { new[] { "ab", string.Empty, "cd", "ef", "gh", "ij", "kl", "mn", string.Empty }, 2 },
+        };
 
     }
 }
