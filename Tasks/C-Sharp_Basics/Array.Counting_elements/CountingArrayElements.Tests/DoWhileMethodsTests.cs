74b99afda3a9d1b15de98c29cbe1ce2b5f51e206
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-15 11:36:58 +0300

    object zero decimal count

--- Tasks/C-Sharp_Basics/Array.Counting_elements/CountingArrayElements.Tests/DoWhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Counting_elements/CountingArrayElements.Tests/DoWhileMethodsTests.cs
@@ -5,6 +5,15 @@ namespace CountingArrayElements.Tests
     [TestFixture]
     public sealed class DoWhileMethodsTests
     {
+        private static readonly object[][] GetZeroDecimalCountData =
+        {
+            new object[] { new[] { 1.1m }, 0 },
+            new object[] { new[] { decimal.Zero }, 1 },
+            new object[] { new[] { decimal.Zero, 1.1m }, 1 },
+            new object[] { new[] { decimal.Zero, decimal.Zero }, 2 },
+            new object[] { new[] { 1.1m, decimal.Zero, 2.2m, decimal.Zero, 3.3m }, 2 },
+            new object[] { new[] { decimal.Zero, 1.1m, decimal.Zero, 2.2m, 3.3m, decimal.Zero, decimal.Zero }, 4 },
+        };
 
     }
 }
