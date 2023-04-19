688492b3065aac7bef65ef5ad55bffb89576a8c6
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-19 08:49:06 +0300

    tests file added

---
+++ Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Inheritance.Tests
+{
+    [TestFixture]
+    public class InheritanceTests
+    {
+
+    }
+}
+
