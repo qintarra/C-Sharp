133cea81891328bfa6932b5040b472365e6b75e0
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-19 08:50:04 +0300

    field exist

--- Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
+++ Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
@@ -19,6 +19,22 @@ namespace Inheritance.Tests
             GetClass(className);
         }
 
+        [TestCase("Employee", "name", typeof(string))]
+        [TestCase("Employee", "salary", typeof(decimal))]
+        [TestCase("Employee", "bonus", typeof(decimal))]
+        [TestCase("Manager", "quantity", typeof(int))]
+        [TestCase("SalesPerson", "percent", typeof(int))]
+        public void FieldExist(string className, string fieldName, Type fieldType)
+        {
+            var classType = GetClass(className);
+            var field = classType.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.Multiple(() =>
+            {
+                Assert.That(field, Is.Not.Null);
+                Assert.That(field.FieldType, Is.EqualTo(fieldType));
+            });
+        }
+
     }
 }
 
