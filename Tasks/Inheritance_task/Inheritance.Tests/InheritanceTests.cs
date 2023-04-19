971da7359c925f7317f589dc6016efd305822d7f
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-19 08:51:47 +0300

    method

--- Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
+++ Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
@@ -65,6 +65,21 @@ namespace Inheritance.Tests
             });
         }
 
+        [TestCaseSource("MethodsData")]
+        public void MethodExist(string className, string methodName, Type returnType, (string, Type)[] parameters)
+        {
+            var classType = GetClass(className);
+            var method = classType.GetMethod(methodName);
+            Assert.Multiple(() =>
+            {
+                Assert.That(method, Is.Not.Null);
+                Assert.That(method.ReturnType, Is.EqualTo(returnType));
+                Assert.That(method.GetParameters()
+                        .Select(x => (x.Name, x.ParameterType)).SequenceEqual(parameters),
+                    Is.True);
+            });
+        }
+
     }
 }
 
