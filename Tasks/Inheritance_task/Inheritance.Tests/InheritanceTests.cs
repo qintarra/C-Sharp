2dd93787a615de0d058243e4bd79d957c0019b7c
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-19 08:49:38 +0300

    class exist

--- Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
+++ Tasks/Inheritance_task/Inheritance.Tests/InheritanceTests.cs
@@ -9,6 +9,15 @@ namespace Inheritance.Tests
     [TestFixture]
     public class InheritanceTests
     {
+        #region low
+
+        [TestCase("Employee")]
+        [TestCase("Manager")]
+        [TestCase("SalesPerson")]
+        public void ClassExist(string className)
+        {
+            GetClass(className);
+        }
 
     }
 }
