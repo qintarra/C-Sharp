1ac7a4ac1cb4cf19ff75762ee517f13a4072bb43
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-21 12:04:02 +0300

    retuns position

--- Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/ForMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/ForMethodsTests.cs
@@ -14,5 +14,18 @@ namespace GettingArrayElementIndex.Tests
             Assert.Throws<ArgumentNullException>(() => GetIndexOf(null, 0));
         }
 
+        [TestCase(new uint[0], 0u, ExpectedResult = -1)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 1, 2, 3 }, 0u, ExpectedResult = 0)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 1, 2, 3 }, 1u, ExpectedResult = 1)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 1, 2, 3 }, 2u, ExpectedResult = 2)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 1, 2, 3 }, 3u, ExpectedResult = 3)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9u, ExpectedResult = 9)]
+        [TestCase(new uint[] { 0, 1, 2, 3, 1, 2, 3 }, 4u, ExpectedResult = -1)]
+        public int GetIndexOf_NonEmptyArray_ReturnsPosition(uint[] arrayToSearch, uint value)
+        {
+            // Act
+            return GetIndexOf(arrayToSearch, value);
+        }
+
     }
 }
