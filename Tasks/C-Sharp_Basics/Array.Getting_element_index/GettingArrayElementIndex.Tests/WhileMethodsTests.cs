8e49aa806790a445cbfc9db69f08249b3bf7a497
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-23 00:02:19 +0300

    while methods tests added

---
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/WhileMethodsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using static GettingArrayElementIndex.WhileMethods;
+
+// ReSharper disable StringLiteralTypo
+namespace GettingArrayElementIndex.Tests
+{
+    [TestFixture]
+    public sealed class WhileMethodsTests
+    {
+
+    }
+}
