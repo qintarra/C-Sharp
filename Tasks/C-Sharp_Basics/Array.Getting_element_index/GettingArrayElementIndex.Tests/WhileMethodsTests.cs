2f89d39b4645627474ae214f943bfdda626d561a
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-24 09:31:40 +0300

    start index exeption

--- Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/WhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/WhileMethodsTests.cs
@@ -119,5 +119,12 @@ namespace GettingArrayElementIndex.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => GetLastIndexOf(Array.Empty<ushort>(), 0, -1, 0));
         }
 
+        [Test]
+        public void GetLastIndexOf_StartIndexCount_StartIndexGreaterArrayLength_ThrowsException()
+        {
+            // Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => GetLastIndexOf(Array.Empty<ushort>(), 0, 1, 0));
+        }
+
     }
 }
