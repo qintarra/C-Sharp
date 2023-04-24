da44c04cb0e322b9a1f0781efefceaa68823c08b
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-24 09:28:24 +0300

    returns position 2

--- Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/WhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/WhileMethodsTests.cs
@@ -55,5 +55,35 @@ namespace GettingArrayElementIndex.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => GetIndexOf(Array.Empty<ushort>(), 0, 0, -1));
         }
 
+        [TestCase(new ushort[0], (ushort)0, 0, 0, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 0, 9, ExpectedResult = 0)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 0, 14, ExpectedResult = 0)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 1, 8, ExpectedResult = 7)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 1, 4, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 7, 1, ExpectedResult = 7)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)0, 9, 5, ExpectedResult = 11)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 0, 9, ExpectedResult = 1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 0, 14, ExpectedResult = 1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 1, 8, ExpectedResult = 1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 1, 4, ExpectedResult = 1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 2, 1, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 6, 1, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 4, 1, ExpectedResult = 4)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)1, 9, 5, ExpectedResult = 12)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 0, 9, ExpectedResult = 3)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 0, 14, ExpectedResult = 3)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 1, 8, ExpectedResult = 3)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 1, 4, ExpectedResult = 3)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 2, 1, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 6, 1, ExpectedResult = 6)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 4, 1, ExpectedResult = -1)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, (ushort)3, 9, 5, ExpectedResult = 10)]
+        [TestCase(new ushort[] { 0, 1, 2, 3, 1, 2, 3 }, (ushort)4, 0, 7, ExpectedResult = -1)]
+        public int GetIndexOf_NonEmptyArray_ReturnsPosition2(ushort[] arrayToSearch, ushort value, int startIndex, int count)
+        {
+            // Act
+            return GetIndexOf(arrayToSearch, value, startIndex, count);
+        }
+
     }
 }
