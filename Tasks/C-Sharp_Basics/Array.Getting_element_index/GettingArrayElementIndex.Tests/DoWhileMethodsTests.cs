01399d00fc5279e680d52890261aae544106555d
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-26 00:32:11 +0300

    start index count position

--- Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/DoWhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/DoWhileMethodsTests.cs
@@ -131,5 +131,34 @@ namespace GettingArrayElementIndex.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => GetLastIndexOf(Array.Empty<ulong>(), 0, 0, -1));
         }
 
+        [TestCase(new ulong[0], 0ul, 0, 0, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 0, 9, ExpectedResult = 7)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 0, 14, ExpectedResult = 11)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 1, 8, ExpectedResult = 7)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 1, 4, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 7, 1, ExpectedResult = 7)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 0ul, 9, 5, ExpectedResult = 11)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 0, 9, ExpectedResult = 8)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 0, 14, ExpectedResult = 12)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 1, 8, ExpectedResult = 8)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 1, 4, ExpectedResult = 4)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 2, 1, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 6, 1, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 4, 1, ExpectedResult = 4)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 1ul, 9, 5, ExpectedResult = 12)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 0, 9, ExpectedResult = 6)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 0, 14, ExpectedResult = 10)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 1, 8, ExpectedResult = 6)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 1, 4, ExpectedResult = 3)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 2, 1, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 6, 1, ExpectedResult = 6)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 4, 1, ExpectedResult = -1)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3 }, 3ul, 9, 5, ExpectedResult = 10)]
+        [TestCase(new ulong[] { 0, 1, 2, 3, 1, 2, 3 }, 4ul, 0, 7, ExpectedResult = -1)]
+        public int GetLastIndexOf_StartIndexCount_NonEmptyArray_ReturnsPosition(ulong[] arrayToSearch, ulong value, int startIndex, int count)
+        {
+            // Act
+            return GetLastIndexOf(arrayToSearch, value, startIndex, count);
+        }
     }
 }
