1f78965dcd5f944b78124fc376d6f63650bd54d6
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-25 14:50:08 +0300

    do while methods tests added

---
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/DoWhileMethodsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using static GettingArrayElementIndex.DoWhileMethods;
+
+// ReSharper disable StringLiteralTypo
+namespace GettingArrayElementIndex.Tests
+{
+    [TestFixture]
+    public sealed class DoWhileMethodsTests
+    {
+
+    }
+}
