05939cf2909a91380f4826d311b7008b8173a129
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-25 14:52:59 +0300

    start index count less zero exeption

--- Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/DoWhileMethodsTests.cs
+++ Tasks/C-Sharp_Basics/Array.Getting_element_index/GettingArrayElementIndex.Tests/DoWhileMethodsTests.cs
@@ -47,5 +47,12 @@ namespace GettingArrayElementIndex.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => GetIndexOf(Array.Empty<ulong>(), 0, 1, 0));
         }
 
+        [Test]
+        public void GetIndexOf_StartIndexCount_CountLessZero_ThrowsException()
+        {
+            // Act
+            Assert.Throws<ArgumentOutOfRangeException>(() => GetIndexOf(Array.Empty<ulong>(), 0, 0, -1));
+        }
+
     }
 }
