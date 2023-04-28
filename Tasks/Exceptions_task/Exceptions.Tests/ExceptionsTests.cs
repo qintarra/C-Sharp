2a855929138ce748f6210a7bba31825941937935
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-28 09:40:57 +0300

    arrays create matrix

--- Tasks/Exceptions_task/Exceptions.Tests/ExceptionsTests.cs
+++ Tasks/Exceptions_task/Exceptions.Tests/ExceptionsTests.cs
@@ -8,7 +8,24 @@ namespace Exceptions.Tests
     {
         #region Startup Data
 
+        #region ArraysCreateMatrix
 
+        private static readonly object[] ArraysCreateMatrix =
+        {
+            new object[] {new double[0, 0] { }},
+            new object[]
+            {
+                new double[3, 4]
+                {
+                    {1, 2, 3, 4},
+                    {1, 2, 3, 4},
+                    {1, 2, 3, 4}
+                }
+            }
+        };
+
+        #endregion
+        
         #endregion
 
 
