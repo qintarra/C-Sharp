03dc2775140dabd3b30b55b6b0fa6886265dd3d6
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-28 09:39:47 +0300

    tests added

---
+++ Tasks/Exceptions_task/Exceptions.Tests/ExceptionsTests.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace Exceptions.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        #region Startup Data
+
+
+        #endregion
+
+
+    }
+}
