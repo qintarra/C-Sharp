7fa343c59968846f03e1abec9912eeb792773645
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-01 00:16:28 +0300

    array empty

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
@@ -24,5 +24,18 @@ namespace LookingForArrayElements.Tests
                 message: "Method throws ArgumentNullException in case an array of elements to for search is null.");
         }
 
+        [Test]
+        public void GetIntegersCount_ArrayToSearchIsEmpty_ReturnZeroOccurrences()
+        {
+            // Arrange
+            int[] elementsToSearchFor = { 2, 5, 8 };
+
+            // Act
+            int actual = IntegersCounter.GetIntegersCount(Array.Empty<int>(), elementsToSearchFor);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
     }
 }
