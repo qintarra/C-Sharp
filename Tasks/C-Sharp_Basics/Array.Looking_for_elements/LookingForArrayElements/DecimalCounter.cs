89edc882a0a57694d37c75c454d3cc37d6ae6761
qintarra <96946215+qintarra@users.noreply.github.com>
2023-04-30 10:52:16 +0300

    foreach and for statements

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements/DecimalCounter.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements/DecimalCounter.cs
@@ -13,7 +13,7 @@ namespace LookingForArrayElements
         /// <returns>The number of occurrences of the <see cref="Array"/> elements that match the range criteria.</returns>
         public static int GetDecimalsCount(decimal[]? arrayToSearch, decimal[]?[]? ranges)
         {
-            // #5. Implement the method using "do..while" statements.
+            // #5. Implement the method using "foreach" statements.
             if (arrayToSearch == null)
             {
                 throw new ArgumentNullException(nameof(arrayToSearch));
@@ -69,7 +69,58 @@ namespace LookingForArrayElements
         public static int GetDecimalsCount(decimal[]? arrayToSearch, decimal[]?[]? ranges, int startIndex, int count)
         {
             // #6. Implement the method using "for" statement.
+            if (arrayToSearch == null)
+            {
+                throw new ArgumentNullException(nameof(arrayToSearch));
+            }
 
+            if (ranges == null)
+            {
+                throw new ArgumentNullException(nameof(ranges));
+            }
+
+            if (startIndex < 0 || startIndex >= arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            }
+
+            if (count < 0 || startIndex + count > arrayToSearch.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            int matchCount = 0;
+
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                decimal valueToMatch = arrayToSearch[i];
+
+                for (int j = 0; j < ranges.Length; j++)
+                {
+                    decimal[]?range = ranges[j];
+
+                    if (range == null)
+                    {
+                        throw new ArgumentNullException(nameof(ranges));
+                    }
+
+                    if (range.Length != 2)
+                    {
+                        throw new ArgumentException("Range array must have exactly two elements.", nameof(ranges));
+                    }
+
+                    decimal rangeStart = range[0];
+                    decimal rangeEnd = range[1];
+
+                    if (valueToMatch >= rangeStart && valueToMatch <= rangeEnd)
+                    {
+                        matchCount++;
+                        break;
+                    }
+                }
+            }
+
+            return matchCount;
         }
     }
 }
