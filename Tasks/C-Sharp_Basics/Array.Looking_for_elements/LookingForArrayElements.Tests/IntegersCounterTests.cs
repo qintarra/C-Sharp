9f5b3e2df838268ecec45a1ff3cc2649d4cc655d
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-01 00:15:19 +0300

    array null exeption

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
@@ -6,6 +6,14 @@ namespace LookingForArrayElements.Tests
     [TestFixture]
     public sealed class IntegersCounterTests
     {
+        [Test]
+        public void GetIntegersCount_ArrayToSearchIsNull_ThrowArgumentNullException()
+        {
+            // Act
+            Assert.Throws<ArgumentNullException>(
+                () => IntegersCounter.GetIntegersCount(null, Array.Empty<int>()),
+                message: "Method throws ArgumentNullException in case an array to search is null.");
+        }
 
     }
 }
