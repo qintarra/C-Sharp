2e15257eaaca481f998fad86955c3e7e23610184
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-03 00:06:50 +0300

    null array exeption

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/FloatCounterTests.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/FloatCounterTests.cs
@@ -8,5 +8,14 @@ namespace LookingForArrayElements.Tests
     {
         private static readonly float[] ArrayWithNineElements = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f };
 
+        [Test]
+        public void GetFloatsCount_ArrayToSearchIsNull_ThrowsArgumentNullException()
+        {
+            // Act
+            Assert.Throws<ArgumentNullException>(
+                () => FloatCounter.GetFloatsCount(null, Array.Empty<float>(), Array.Empty<float>()),
+                message: "Method throws ArgumentNullException in case an array to search is null.");
+        }
+
     }
 }
