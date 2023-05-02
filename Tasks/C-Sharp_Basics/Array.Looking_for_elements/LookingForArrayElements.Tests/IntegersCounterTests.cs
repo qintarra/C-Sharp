ab50ba5f33b6835cf4bbf94fff93cc7eb4a8ecda
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-02 09:13:27 +0300

    valid

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/IntegersCounterTests.cs
@@ -50,5 +50,16 @@ namespace LookingForArrayElements.Tests
             Assert.AreEqual(0, actual);
         }
 
+        [TestCase(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 2, 5, 8 }, ExpectedResult = 3)]
+        [TestCase(new[] { 1, 2, 3, 2, 4, 5, 6, 5, 5, 8, 7, 8, 9 }, new[] { 2, 5, 8 }, ExpectedResult = 7)]
+        [TestCase(new[] { 1, 2, 3, 2, 4, 5, 6, 5, 5, 8, 7, 8, 9 }, new[] { -2, -5, -8 }, ExpectedResult = 0)]
+        [TestCase(new[] { 5, 4, 21, 38, 25, int.MaxValue, 48, 98, 14, 43, 11, 6, 81, 532, 58 }, new[] { int.MaxValue, 47, 14, 6, 532, int.MinValue }, ExpectedResult = 4)]
+        [TestCase(new[] { -5, 4, -21, 38, -25, int.MinValue, 48, -98, 14, -43, 11, -6, 81, -532, -58 }, new[] { int.MaxValue, -47, 14, -6, 532, int.MinValue }, ExpectedResult = 3)]
+        public int GetIntegersCount_ParametersAreValid_ReturnsResult(int[] arrayToSearch, int[] elementsToSearchFor)
+        {
+            // Act
+            return IntegersCounter.GetIntegersCount(arrayToSearch, elementsToSearchFor);
+        }
+
     }
 }
