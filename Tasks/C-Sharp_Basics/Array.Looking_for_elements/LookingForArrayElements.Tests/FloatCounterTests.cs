42c2dc75983fb9dae60a66b82798811c173a7cef
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-03 00:06:07 +0300

    float counter tests created

---
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/FloatCounterTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LookingForArrayElements.Tests
+{
+    [TestFixture]
+    public sealed class FloatCounterTests
+    {
+        private static readonly float[] ArrayWithNineElements = { 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f };
+
+    }
+}
