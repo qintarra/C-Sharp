e49cfd04fd396bb20912cfcecefc5e2da3f5aa5a
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-08 09:33:53 +0300

    decimal counter tests

---
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/DecimalCounterTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace LookingForArrayElements.Tests
+{
+    [TestFixture]
+    public sealed class DecimalCounterTests
+    {
+        private static readonly decimal[] ArrayWithFiveElements = { 0.1m, 0.2m, 0.3m, 0.4m, 0.5m };
+        private static readonly decimal[] ArrayWithFifteenElements = { decimal.MaxValue, -0.1m, -0.2m, decimal.One, -0.3m, -0.4m, -0.5m, decimal.Zero, 0.1m, 0.2m, 0.3m, 0.4m, 0.5m, decimal.MinusOne, decimal.MinValue };
+
+    }
+}
