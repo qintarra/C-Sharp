f558c54957f36ecfdc4d6156acc6df06969153db
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-08 09:34:27 +0300

    array null exeption

--- Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/DecimalCounterTests.cs
+++ Tasks/C-Sharp_Basics/Array.Looking_for_elements/LookingForArrayElements.Tests/DecimalCounterTests.cs
@@ -8,5 +8,14 @@ namespace LookingForArrayElements.Tests
         private static readonly decimal[] ArrayWithFiveElements = { 0.1m, 0.2m, 0.3m, 0.4m, 0.5m };
         private static readonly decimal[] ArrayWithFifteenElements = { decimal.MaxValue, -0.1m, -0.2m, decimal.One, -0.3m, -0.4m, -0.5m, decimal.Zero, 0.1m, 0.2m, 0.3m, 0.4m, 0.5m, decimal.MinusOne, decimal.MinValue };
 
+        [Test]
+        public void DecimalCounter_ArrayToSearchIsNull_ThrowArgumentNullException()
+        {
+            // Act
+            Assert.Throws<ArgumentNullException>(
+                () => DecimalCounter.GetDecimalsCount(null, Array.Empty<decimal[]>()),
+                message: "Method throws ArgumentNullException in case array to search is null.");
+        }
+
     }
 }
