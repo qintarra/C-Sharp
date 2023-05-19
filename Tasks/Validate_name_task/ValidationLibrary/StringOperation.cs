34589624413d805c3c0eee072f7579a95c6c8413
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-19 21:41:30 +0300

    cs file added

---
+++ Tasks/Validate_name_task/ValidationLibrary/StringOperation.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Linq;
+
+namespace ValidationLibrary
+{
+    public static class StringOperation
+    {
+        // Create static method 'GetValidName' with 'nameToValidate' parameter 
+        public static string GetValidName(string nameToValidate)
+        {
+
+        }
+    }
+}
