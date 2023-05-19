db245fd352914e305b3fc64c6db677e90a3dd24a
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-19 21:44:51 +0300

    tests file added

---
+++ Tasks/Validate_name_task/ValidationLibrary.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ValidationLibrary.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+    }
+}
