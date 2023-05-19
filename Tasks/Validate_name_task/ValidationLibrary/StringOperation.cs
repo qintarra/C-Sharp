3ec403d461667b575868c3bd86e56d6d9c9995cc
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-19 21:42:08 +0300

    GetValidName method implemented

--- Tasks/Validate_name_task/ValidationLibrary/StringOperation.cs
+++ Tasks/Validate_name_task/ValidationLibrary/StringOperation.cs
@@ -9,7 +9,25 @@ namespace ValidationLibrary
         // Create static method 'GetValidName' with 'nameToValidate' parameter 
         public static string GetValidName(string nameToValidate)
         {
+            if (string.IsNullOrEmpty(nameToValidate))
+            {
+                throw new ArgumentException("Input string is missing or zero-length.");
+            }
 
+            var words = nameToValidate.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(w => new string(w.Where(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')).ToArray()))
+                .Where(w => !string.IsNullOrEmpty(w))
+                .Select(w => w[..1].ToUpper() + w[1..].ToLower())
+                .ToList();
+
+            if (words.Count == 0)
+            {
+                throw new ArgumentException("Output string is empty.");
+            }
+
+            var validName = string.Join(" ", words)[..Math.Min(string.Join(" ", words).Length, 50)];
+
+            return validName;
         }
     }
 }
