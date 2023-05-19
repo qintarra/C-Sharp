6f2ba80b36b4dc9810b6b983c50877a7df074dce
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-19 21:45:08 +0300

    set up

--- Tasks/Validate_name_task/ValidationLibrary.Tests/Tests.cs
+++ Tasks/Validate_name_task/ValidationLibrary.Tests/Tests.cs
@@ -8,6 +8,17 @@ namespace ValidationLibrary.Tests
     [TestFixture]
     public class Tests
     {
+        private Type type;
+        private Type paramType;
+        private string methodName;
+
+        [SetUp]
+        public void Initial()
+        {
+            type = Type.GetType("ValidationLibrary.StringOperation, ValidationLibrary", false, true);
+            paramType = typeof(string);
+            methodName = "GetValidName";
+        }
 
     }
 }
