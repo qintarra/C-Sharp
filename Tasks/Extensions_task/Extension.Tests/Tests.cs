030842f32427823d0ae9c1c2ce45d5bff085c803
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-21 10:19:53 +0300

    extension tests added

---
+++ Tasks/Extensions_task/Extension.Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Extension.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+    }
+}
