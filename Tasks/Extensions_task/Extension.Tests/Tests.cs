761094faebbaffcc998d58cac466ff2786eb1412
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-21 10:20:19 +0300

    summa digit

--- Tasks/Extensions_task/Extension.Tests/Tests.cs
+++ Tasks/Extensions_task/Extension.Tests/Tests.cs
@@ -8,6 +8,19 @@ namespace Extension.Tests
     [TestFixture]
     public class Tests
     {
+        [TestCase(1074, 12)]
+        [TestCase(-4678, 25)]
+        [TestCase(0, 0)]
+        [TestCase(02, 2)]
+        [TestCase(333333, 18)]
+        public void SummaDigit_WithIntegerValue_ReturntSumOfDigits(int n, int expected)
+        {
+            //Act 
+            var actualResult = n.SummaDigit();// MyExtensions.SummaDigit(n);
+
+            //Assert 
+            Assert.AreEqual(expected, actualResult, message: "SummaDigit method works incorrect ");
+        }
 
     }
 }
