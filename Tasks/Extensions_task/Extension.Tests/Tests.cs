0be1d3baf1fa75a3221b40a17688cbd65b2a5963
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-21 10:20:38 +0300

    summa with reverse

--- Tasks/Extensions_task/Extension.Tests/Tests.cs
+++ Tasks/Extensions_task/Extension.Tests/Tests.cs
@@ -22,5 +22,18 @@ namespace Extension.Tests
             Assert.AreEqual(expected, actualResult, message: "SummaDigit method works incorrect ");
         }
 
+        [TestCase((uint)132, (ulong)363)]
+        [TestCase((uint)9, (ulong)18)]
+        [TestCase((uint)0, (ulong)0)]
+        [TestCase((uint)8421, (ulong)9669)]
+        [TestCase((uint)74, (ulong)121)]
+        public void SummaWithReverse_WithUintValue_ReturnSum(uint n, ulong expected)
+        {
+            //Act 
+            var actualResult = n.SummaWithReverse();// MyExtensions.SummaWithReverse(n);
+                                                    //Assert
+            Assert.AreEqual(expected, actualResult, message: "SummaWithReverse works incorrect");
+        }
+
     }
 }
