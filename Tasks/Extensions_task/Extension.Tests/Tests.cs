6092ea4bfe6f7bac8a92cdfb3e053497bb81d843
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-22 08:04:09 +0300

    count not letter

--- Tasks/Extensions_task/Extension.Tests/Tests.cs
+++ Tasks/Extensions_task/Extension.Tests/Tests.cs
@@ -31,9 +31,60 @@ namespace Extension.Tests
         {
             //Act 
             var actualResult = n.SummaWithReverse();// MyExtensions.SummaWithReverse(n);
-                                                    //Assert
+			
+            //Assert
             Assert.AreEqual(expected, actualResult, message: "SummaWithReverse works incorrect");
         }
 
+        [TestCase("I like C#", 3)]
+        [TestCase("Hello мир", 4)]
+        [TestCase("", 0)]
+        [TestCase("Пр4об5е9л",9 )]
+        [TestCase("Hellouniverse", 0)]
+        [TestCase("q7er51ty6i", 4)]
+        [TestCase("@8/-u=", 5)]
+        public void CountNotLetter_WithStringValue_ReturnAmountOfNonAlphabet(string str, int expected)
+        {
+            //Act
+            var actualResult = str.CountNotLetter();// MyExtensions.CountNotLetter(str);
+			
+            //Assert
+            Assert.AreEqual(expected, actualResult, message: "CountNotLetter works incorrect ");
+        }
+
+        [TestCase(DayOfWeek.Monday, false)]
+        [TestCase(DayOfWeek.Tuesday, false)]
+        [TestCase(DayOfWeek.Wednesday, false)]
+        [TestCase(DayOfWeek.Thursday, false)]
+        [TestCase(DayOfWeek.Friday, false)]
+        [TestCase(DayOfWeek.Saturday, true)]
+        [TestCase(DayOfWeek.Sunday, true)]
+        public void IsDayOff_WithDayOfWeekValue_ReturnCorrectValue(DayOfWeek day, bool expected)
+        {
+            //Act
+            var actualResult = day.IsDayOff();// MyExtensions.IsDayOff(day);
+			
+            //Assert
+            Assert.AreEqual(expected, actualResult, message: "IsDayOff works incorrect");
+        }
+
+        [Test,TestCaseSource("Collection")]
+        public void EvenPositiveElements_WithCollectionParameter_ReturnEvenNumbers(IEnumerable<int> numbers,IEnumerable<int> expected)
+        {
+            //Act
+            var actualResult = numbers.EvenPositiveElements();// MyExtensions.EvenPositiveElements(numbers);
+			
+            //Assert
+            CollectionAssert.AreEqual(expected, actualResult, message: "EvenPositiveElements works incorrect ");           
+        }
+
+        static object[] Collection =
+        {
+            new object []{new int[]{5,1,8,24}, new int[] { 8,24} },
+            new object []{new List<int>() { 2,2,2,5,47,18}, new List<int>() { 2, 2, 2, 18 } },
+            new object[]{ new List<int>() { 3,7,0,1,0},new List<int>() },
+            new object[]{ new int[] { 2, -2, 3, -7, 9, 12, 4, 9 },new List<int>(){2,12,4} },
+            new object[]{ new List<int>() { 1, 3, -4, 11, 5, 18 } ,new int[] { 18}  }
+        };
     }
 }
