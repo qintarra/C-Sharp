ff068a043ae577ccea1bd70dd067cad82ef7ff0c
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-26 19:28:55 +0300

    middle region

--- Tasks/LINQ_task/Linq.Tests/Tests.cs
+++ Tasks/LINQ_task/Linq.Tests/Tests.cs
@@ -182,6 +182,162 @@ namespace Linq.Tests
 
         #endregion
 
+        #region Middle
+        [Test]
+        public void Task6Test()
+        {
+            foreach (var (numbers, stringList, expected) in Task6Data())
+            {
+                var actualResult = Tasks.Task6(numbers, stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected.OrderBy(x => x), actualResult.OrderBy(x => x));
+            }
+        }
+
+        [Test]
+        public void Task7Test()
+        {
+            foreach (var (k, integerList, expected) in Task7Data())
+            {
+                var actualResult = Tasks.Task7(k, integerList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+        [Test]
+        public void Task8Test()
+        {
+            foreach (var (k, d, integerList, expected) in Task8Data())
+            {
+                var actualResult = Tasks.Task8(k, d, integerList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+        [Test]
+        public void Task9Test()
+        {
+            foreach (var (stringList, expected) in Task9Data())
+            {
+                var actualResult = Tasks.Task9(stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+        [Test]
+        public void Task10Test()
+        {
+            foreach (var (stringList, expected) in Task10Data())
+            {
+                var actualResult = Tasks.Task10(stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+        private IEnumerable<(IEnumerable<int> numbers, IEnumerable<string> stringList, IEnumerable<string> expected)> Task6Data()
+        {
+            yield return (
+                numbers: new []{ 4, 1, 6 },
+                stringList: new []{ "qwer", "5qwer", "3qwe", "7qwert", "q" },
+                expected: new []{ "3qwe", "Not found", "7qwert" });
+            yield return (
+                numbers: new[] { 4, 1, 6, 3, 12 },
+                stringList: new[] { "2qwe", "4qwe", "5", "3qwe", "7qwert", "1 the 12long" },
+                expected: new[] { "2qwe", "5", "7qwert", "Not found", "1 the 12long" });
+            yield return (
+                numbers: new[] { 7, 1, Int32.MaxValue },
+                stringList: new[] { "qwer", "5qwer", "3qwe", "0qwerty" },
+                expected: new[] { "0qwerty", "Not found", "Not found" });
+            yield return (
+                numbers: new[] { 44, 21, 78 },
+                stringList: new[] { "qwer", "5qwer", "3qwe", "7qwert" },
+                expected: new[] { "Not found", "Not found", "Not found" });
+        }
+
+        private IEnumerable<(int k, IEnumerable<int> integerList, IEnumerable<int> expected)> Task7Data()
+        {
+            yield return (
+                k: 2,
+                integerList: new[] { 1, 3, 78, 96, 76, 45, 5, -89, 56, 41 },
+                expected: new List<int>());
+            yield return (
+                k: 20,
+                integerList: new[] { 1, 3, 76, 96, 78, 45, 3, -89, 56, 41 },
+                expected: new[] { 56, 78, 96, 76 } );
+            yield return (
+                k: 5,
+                integerList: new[] { 1, 12, 14, 7, 92, 5, 423, 11, 47, 71, 43 },
+                expected: new [] {92, 14, 12});
+            yield return (
+                k: 7,
+                integerList: new[] { 1, 73, 45, 87, 45, 19, 13, 44, 76, -78 },
+                expected: new List<int>());
+        }
+        
+        private IEnumerable<(int k, int d, IEnumerable<int> integerList, IEnumerable<int> expected)> Task8Data()
+        {
+            yield return (
+                k: 3,
+                d: 25,
+                integerList: new[] { 12, 17, 18, 19, 25, 14, 45, 71, 63, 78, 11 },
+                expected: new[] { 78, 71, 63, 45, 25, 19, 18, 17, 14, 12, 11 });
+            yield return (
+                k: 30,
+                d: 1,
+                integerList: new[] { 12, 17, 18, 19, 25, 14, 45, 71, 63, 78, 11 },
+                expected: new List<int>());
+            yield return (
+                k: 8,
+                d: 1,
+                integerList: new[] { 12, 17, 18, 19, 25, 14, 45, 71, 63, 78, 11 },
+                expected: new [] { 78, 63, 11 });
+            yield return (
+                k: 15,
+                d: 25,
+                integerList: new[] { 12, 17, 18, 19, 25, 14, 45, 71, 63, 78, 11 },
+                expected: new[] { 25, 19, 18, 17, 14, 12 });
+        }
+        
+        private IEnumerable<(IEnumerable<string> stringList, IEnumerable<string> expected)> Task9Data()
+        {
+            yield return (
+                stringList: new[] { "YELLOW", "GREEN", "YIELD" },
+                expected: new[] { "11-Y", "5-G" });
+            yield return (
+                stringList: new[] { "FIREE", "FIGHTER", "AGGREGATE", "ACE" },
+                expected: new[] { "12-A", "12-F" });
+            yield return (
+                stringList: new[]
+                {
+                    "EPAM", "GOOGLE", "MICROSOFT", "APPLE", "TESLA", "STARLINK", "TWITTER"
+                },
+                expected: new []
+                {
+                    "12-T", "9-M", "8-S", "6-G", "5-A", "4-E"
+                });
+        }
+
+        private IEnumerable<(IEnumerable<string> stringList, IEnumerable<string> expected)> Task10Data()
+        {
+            yield return (
+                stringList: new[] { "bc", "sd", "ac", "sdf", "ewr" },
+                expected: new[] { "CCD", "RF" });
+            yield return (
+                stringList: new[] { "du", "the", "ace", "run", "cut" },
+                expected: new[] { "ETNE", "U" });
+            yield return (
+                stringList: new[] { "ab", "attribute", "cheese", "swim", "cut" },
+                expected: new[] { "B", "E", "E", "T", "M" });
+            yield return (
+                stringList: new[] { "star", "galaxy", "quasar", "planet", "asteroid", "satellite", "comet" },
+                expected: new[] { "YTR", "D", "T", "E", "R" });
+        }
+
+        #endregion
+
         #region Utility
 
         private void AssertIsLinq<T>(IEnumerable<T> result)
