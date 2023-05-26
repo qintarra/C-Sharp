e74e97f70122e9dbb49e2b8bc391ea935e795935
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-26 19:27:17 +0300

    low region tests

--- Tasks/LINQ_task/Linq.Tests/Tests.cs
+++ Tasks/LINQ_task/Linq.Tests/Tests.cs
@@ -15,7 +15,66 @@ namespace Linq.Tests
         private readonly MaxDiscountOwnerComparer _discountOwnerComparer = new MaxDiscountOwnerComparer();
         private readonly CountryStatComparer _countryStatComparer = new CountryStatComparer();
 
+        #region Low
 
+        [Test]
+        public void Task1Test()
+        {
+            foreach (var (c, stringList, expected) in Task1Data())
+            {
+                var actualResult = Tasks.Task1(c, stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected.OrderBy(x => x), actualResult.OrderBy(x => x));
+            }
+        }
+
+        [Test]
+        public void Task2Test()
+        {
+            foreach (var (stringList, expected) in Task2Data())
+            {
+                var actualResult = Tasks.Task2(stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+        [Test]
+        public void Task3Test()
+        {
+            foreach (var (stringList, expected) in Task3Data())
+            {
+                var actualResult = Tasks.Task3(stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected.OrderBy(x => x), actualResult.OrderBy(x => x));
+            }
+        }
+
+        [Test]
+        public void Task4Test()
+        {
+            foreach (var (k, stringList, expected) in Task4Data())
+            {
+                var actualResult = Tasks.Task4(k, stringList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+        [Test]
+        public void Task5Test()
+        {
+            foreach (var (integerList, expected) in Task5Data())
+            {
+                var actualResult = Tasks.Task5(integerList);
+                AssertIsLinq(actualResult);
+                AssertIsAsExpected(expected, actualResult);
+            }
+        }
+
+
+
+        #endregion
 
         #region Utility
 
