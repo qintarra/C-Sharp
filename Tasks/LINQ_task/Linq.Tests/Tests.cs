9714a08b42f0c7c9240aff0bce13de8391418846
qintarra <96946215+qintarra@users.noreply.github.com>
2023-05-26 19:25:54 +0300

    tests file added

---
+++ Tasks/LINQ_task/Linq.Tests/Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Linq.Objects;
+using NUnit.Framework;
+using Linq.Tests.Comparers;
+
+namespace Linq.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+
+    }
+}
